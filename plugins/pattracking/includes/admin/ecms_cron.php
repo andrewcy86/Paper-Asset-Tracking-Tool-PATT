bdbf1bb159c5ce5fc418115f7919b0d7ce45ebb9
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-04-10 00:15:29 -0400

    Update ecms_cron.php
    Updated CURL code and checking for 401 E_BAD_CREDENTIALS_ERROR.

--- plugins/pattracking/includes/admin/ecms_cron.php
+++ plugins/pattracking/includes/admin/ecms_cron.php
@@ -28,13 +28,20 @@ echo '<strong>Temp Storage Location & Filename:</strong> http://086.info/wordpre
 echo '<strong>PATT Folder/Document ID:</strong> '. $item->folderdocinfo_id;
 echo '<hr />';
 
-$target_url = 'http://lippizzan3.rtpnc.epa.gov:8080/apiman-gateway/ecms/save/1.2?apiKey=[Insert Key]';
-$file_name_with_full_path ='test_pdf.pdf';
+//POST Request to Content Ingestion Endpoint
+
+$target_url = 'http://lippizzan3.rtpnc.epa.gov:8080/apiman-gateway/ecms/save/1.2?apiKey=XXXX';
+//Username.
+$username = 'xxxx';
+ 
+//Password.
+$password = 'xxxx';
+
+$file_name_with_full_path ='final_2017_cgp_current_as_of_6-6-2019.pdf';
 
 //$cfile = new CURLFile($file_name_with_full_path,mime_content_type($file_name_with_full_path),'imported_pdf');    
 // Assign POST data
 
-// POST Request to Content Ingestion Endpoint
 $fileHandler = fopen($file_name_with_full_path, 'r');
 $fileData = fread($fileHandler, filesize($file_name_with_full_path));
 
@@ -42,14 +49,14 @@ $post = array(
 'metadata' => '{ 
 "properties":{ 
 "r_object_type":"erma_content",
-"object_name":"test_pdf.pdf",
+"object_name":"final_2017_cgp_current_as_of_6-6-2019.pdf",
 "a_application_type":"PATT",
-"erma_content_title":"Transit",
+"erma_content_title":"NPDES",
 "erma_content_unid":"0000001a",
 "erma_content_date":"2012-09-12T21:45:32",
 "erma_content_schedule":"000_000_a",
 "erma_content_eventdate":"2013-09-12T21:45:32",
-"erma_sensitivity_id":"3",
+"erma_sensitivity_id":"0",
 "erma_custodian":"ayuen",
 "erma_folder_path":""
 }
@@ -57,21 +64,39 @@ $post = array(
 ','content'=> $fileData);
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL,$target_url);
+curl_setopt($curl, CURLOPT_USERPWD, $username.":".$password);
 curl_setopt($curl, CURLOPT_INFILE, $fileHandler);
 curl_setopt($curl, CURLOPT_INFILESIZE, filesize($file_name_with_full_path));
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER,false);
 curl_setopt($curl, CURLOPT_POST,1);
 curl_setopt($curl, CURLOPT_POST, count($post));
 curl_setopt($curl, CURLOPT_POSTFIELDS, $post);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
 curl_setopt($curl, CURLOPT_VERBOSE,true);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array('[Insert Authroization]'));
+curl_setopt($curl, CURLINFO_HEADER_OUT, true);
 $result = curl_exec($curl);
+$retry = 0;
+$json = json_decode($result, true);
+//print_r($json);
+
+if (array_key_exists("status",$json) || array_key_exists("code",$json))
+  {
+while($json['status'] == 401 && $json['code'] == 'E_BAD_CREDENTIALS_ERROR' && $retry < 3){
+    $result = curl_exec($curl);
+    $retry++;
+}
+  }
+
 if(!$result){
     die('Error: "' . curl_error($curl) . '" - Code: ' . curl_errno($curl));
 }
+
+$information = curl_getinfo($curl);
+print_r( $information);
+
 curl_close($curl);
 var_dump($result);
+
+// Error Handling
 
 // Check Response success 
 if ($response = '') {

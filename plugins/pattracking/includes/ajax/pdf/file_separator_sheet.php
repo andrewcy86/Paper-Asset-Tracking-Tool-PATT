2b73456539d14295beb3d5916fd07857f465cd82
stephanieschouw <51790259+stephanieschouw@users.noreply.github.com>
2020-04-23 07:39:20 -0400

    Fixed label positioning
    Label positioning now aligned to printed out version of Avery labels, now needs real world testing.

--- plugins/pattracking/includes/ajax/pdf/file_separator_sheet.php
+++ plugins/pattracking/includes/ajax/pdf/file_separator_sheet.php
@@ -15,13 +15,13 @@ if (isset($_GET['id']))
     //Set overall values for PDF
     $obj_pdf = new TCPDF('P', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
     $obj_pdf->SetCreator(PDF_CREATOR);
-    $obj_pdf->SetTitle("Folder/File Labels - Paper Asset Tracking Tool");
+    $obj_pdf->SetTitle("File Labels - Paper Asset Tracking Tool");
     $obj_pdf->SetHeaderData('', '', PDF_HEADER_TITLE, PDF_HEADER_STRING);
     $obj_pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN,'',PDF_FONT_SIZE_MAIN));
     $obj_pdf->setFooterFont(Array(PDF_FONT_NAME_DATA,'',PDF_FONT_SIZE_DATA));
     $obj_pdf->SetDefaultMonospacedFont('helvetica');
     $obj_pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-    $obj_pdf->SetMargins(PDF_MARGIN_LEFT, '10', PDF_MARGIN_RIGHT);
+    //$obj_pdf->SetMargins(PDF_MARGIN_LEFT, '10', PDF_MARGIN_RIGHT);
     $obj_pdf->setPrintHeader(false);
     $obj_pdf->setPrintFooter(false);
     $obj_pdf->SetAutoPageBreak(true, 10);
@@ -30,34 +30,14 @@ if (isset($_GET['id']))
 if ((preg_match('/^\d+$/', $GLOBALS['id'])) || (preg_match("/^([0-9]{7}-[0-9]{1,4}-02-[0-9]{1,4})(?:,\s*(?1))*$/", $GLOBALS['id']))) {
 
 if (preg_match('/^\d+$/', $GLOBALS['id'])) {
-
-    // $box_ids = $wpdb->get_results("SELECT id FROM wpqa_wpsc_epa_boxinfo WHERE index_level = 2 AND ticket_id =" .$GLOBALS['id']);
-
-    $args = [
-        'select' => 'id',
-        'where' => [
-                    ['index_level', 2],
-                    ['ticket_id', $GLOBALS['id'], 'AND']
-                ],
-    ];
-    $wpqa_wpsc_epa_boxinfo = new WP_CUST_QUERY('wpqa_wpsc_epa_boxinfo');
-    $box_ids = $wpqa_wpsc_epa_boxinfo->get_results($args, false);
-
+    $box_ids = $wpdb->get_results("SELECT id FROM wpqa_wpsc_epa_boxinfo WHERE index_level = 2 AND ticket_id =" .$GLOBALS['id']);
 
     foreach($box_ids as $item)
     {
 
-// $folderfile_info = $wpdb->get_results("SELECT folderdocinfo_id, title
-// FROM wpqa_wpsc_epa_folderdocinfo
-// WHERE box_id = " .$item->id);
-
-    $args = [
-        'select' => 'folderdocinfo_id, title',
-        'where' => ['box_id', $item->id]
-    ];
-    $wpqa_wpsc_epa_folderdocinfo = new WP_CUST_QUERY('wpqa_wpsc_epa_folderdocinfo');
-    $folderfile_info = $wpqa_wpsc_epa_folderdocinfo->get_results($args, false);
-
+$folderfile_info = $wpdb->get_results("SELECT folderdocinfo_id, title
+FROM wpqa_wpsc_epa_folderdocinfo
+WHERE box_id = " .$item->id);
 
 $maxcols = 3;
 $i = 0;
@@ -68,14 +48,26 @@ $batch = array_chunk($folderfile_info, $batch_of);
 //print_r($batch);
 foreach($batch as $b) {
 
+//set table margins
+$obj_pdf->SetMargins(8,16,0);
+//$obj_pdf->SetHeaderMargin(10);
+//$obj_pdf->SetFooterMargin(16);
+
 //Open the table and its first row
-$tbl = '<table style="width: 638px;" cellspacing="2"; nobr="true">';
+
+$tbl   =  '<style>
+                .tableWithOuterBorder{
+                    border-spacing: 80px 2px;
+                }
+                </style>';
+                
+$tbl .= '<table class="tableWithOuterBorder" style="width: 638px; font-size: 9px;" cellspacing="10" nobr="true">';
 $tbl .= '<tr>';
 
 foreach($b as $info){
 
     $folderfile_id = $info->folderdocinfo_id;
-    $folderfile_barcode =  $obj_pdf->serializeTCPDFtagParameters(array($folderfile_id, 'C128', '', '', 62, 20, 0.4, array('position'=>'S', 'border'=>false, 'padding'=>1, 'fgcolor'=>array(0,0,0), 'bgcolor'=>array(255,255,255), 'text'=>true, 'font'=>'helvetica', 'fontsize'=>8, 'stretchtext'=>4), 'N'));
+    $folderfile_barcode =  $obj_pdf->serializeTCPDFtagParameters(array($folderfile_id, 'C128', '', '', 57, 17, 0.4, array('position'=>'S', 'border'=>false, 'padding'=>1, 'fgcolor'=>array(0,0,0), 'bgcolor'=>array(255,255,255), 'text'=>true, 'font'=>'helvetica', 'fontsize'=>8, 'stretchtext'=>4), 'N'));
     $folderfile_title = $info->title;
     $folderfile_title_truncate = (strlen($folderfile_title) > 30) ? substr($folderfile_title, 0, 30) . '...' : $folderfile_title;
 
@@ -118,17 +110,9 @@ $folderfile_array= explode(',', $GLOBALS['id']);
 
 foreach($folderfile_array as $item) {
 
-// $folderfile_info = $wpdb->get_row("SELECT folderdocinfo_id, title
-// FROM wpqa_wpsc_epa_folderdocinfo
-// WHERE folderdocinfo_id = '" .$item."'");
-
-    $args = [
-        'select' => 'folderdocinfo_id, title',
-        'where' => ['folderdocinfo_id', "$item"]
-    ];
-    $wpqa_wpsc_epa_folderdocinfo = new WP_CUST_QUERY('wpqa_wpsc_epa_folderdocinfo');
-    $folderfile_info = $wpqa_wpsc_epa_folderdocinfo->get_results($args, false);
-
+$folderfile_info = $wpdb->get_row("SELECT folderdocinfo_id, title
+FROM wpqa_wpsc_epa_folderdocinfo
+WHERE folderdocinfo_id = '" .$item."'");
 
 $parent = new stdClass;
 $parent->folderdocinfo_id = $folderfile_info->folderdocinfo_id;
@@ -148,8 +132,8 @@ $batch = array_chunk($final_array, $batch_of);
 foreach($batch as $b) {
 
 //Open the table and its first row
-$tbl = '<table style="width: 638px;" cellspacing="2"; nobr="true">';
-$tbl .= '<tr>';
+$tbl = '<table style="width: 638px;font-size: 9px;" cellspacing="10"; nobr="true">';
+$tbl .= '<tr">';
 
 foreach($b as $info){
 

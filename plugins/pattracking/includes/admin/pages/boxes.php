eca122ba009a85690adaa9ce31c7e252a71027e3
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-04-25 08:28:48 -0400

    Update boxes.php
    Dynamically generated box dashboard leveraging datatables. Box ID filters use bootstrap tags input.

--- plugins/pattracking/includes/admin/pages/boxes.php
+++ plugins/pattracking/includes/admin/pages/boxes.php
@@ -1,169 +1,161 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) {
-  exit; // Exit if accessed directly
+	exit; // Exit if accessed directly
 }
 
-/*wp_enqueue_scripts('wpsc-public', WPPATT_PLUGIN_URL.'asset/js/public.js?version='.WPSC_VERSION, array('jquery'));
-wp_enqueue_scripts('wpsc-modal', WPPATT_ABSPATH.'asset/js/modal.js?version='.WPSC_VERSION, array('jquery'));*/
+global $wpdb, $current_user, $wpscfunction;
 
-/*echo '<script type="text/javascript" href="' . WPPATT_PLUGIN_URL . 'asset/js/admin.js"/>';
-echo '<script type="text/javascript" href="' . WPPATT_PLUGIN_URL . 'asset/js/public.js"/>';
-echo '<script type="text/javascript" href="' . WPPATT_PLUGIN_URL . 'asset/js/modal.js"/>';
-echo '<link rel="stylesheet" type="text/css" href="' . WPPATT_PLUGIN_URL . 'asset/css/bootstrap-iso.css"/>';
-echo '<link rel="stylesheet" type="text/css" href="' . WPPATT_PLUGIN_URL . 'asset/css/public.css"/>';
-echo '<link rel="stylesheet" type="text/css" href="' . WPPATT_PLUGIN_URL . 'asset/css/admin.css"/>';
-echo '<link rel="stylesheet" type="text/css" href="' . WPPATT_PLUGIN_URL . 'asset/css/modal.css"/>'; */
+$GLOBALS['id'] = $_GET['id'];
 
-
-// register jquery and style on initialization
-/*add_action('init', 'register_script');
-function register_script() {
-    wp_register_script('public-js', WPPATT_PLUGIN_URL . 'asset/js/public.js', '', '', false);
-
-   // wp_register_style( 'new_style', plugins_url('/css/new-style.css', __FILE__), false, '1.0.0', 'all');
-}
-
-// use the registered jquery and style above
-add_action('wp_enqueue_scripts', 'enqueue_style');
-
-function enqueue_style(){
-   wp_enqueue_script('public-js');
-
-  // wp_enqueue_style( 'new_style' );
-}
-
-
-function add_my_css_and_my_js_files(){
-        wp_enqueue_script('your-script-name', $this->urlpath  . '/your-script-filename.js', array('jquery'), '1.2.3', true);
-        wp_enqueue_style( 'your-stylesheet-name', plugins_url('/css/new-style.css', __FILE__), false, '1.0.0', 'all');
-    }
-    add_action('wp_enqueue_scripts', "add_my_css_and_my_js_files");*/
-
-
-// register jquery and style on initialization
-/*add_action('init', 'register_script');
-function register_script() {
-    wp_register_script( 'custom_jquery', plugins_url('/js/custom-jquery.js', __FILE__), array('jquery'), '2.5.1' );
-
-    wp_register_style( 'new_style', plugins_url('/css/new-style.css', __FILE__), false, '1.0.0', 'all');
-}
-
-// use the registered jquery and style above
-add_action('wp_enqueue_scripts', 'enqueue_style');
-
-function enqueue_style(){
-   wp_enqueue_script('custom_jquery');
-
-   wp_enqueue_style( 'new_style' );
-}
-*/
-
-
-//Register The scripts 
-// wp_enqueue_script('jquery');
-
-wp_register_script('admin-js', WPPATT_PLUGIN_URL . 'asset/js/admin.js', '', '', false);
-wp_register_script('public-js', WPPATT_PLUGIN_URL . 'asset/js/pattpublic.js', '', '', false);
-wp_register_script('modal-js', WPPATT_PLUGIN_URL . 'asset/js/modal.js', '', '', true);
-// wp_register_script('dataTables-responsive-js', 'https://cdn.datatables.net/responsive/2.2.3/js/responsive.dataTables.min.js
-// ', '', '', false);
-// wp_register_script('customScriptDatatables', plugins_url('pattracking/includes/admin/js/customScriptDatatables.js', '', false));
-
-wp_enqueue_script('admin-js', WPPATT_PLUGIN_URL . 'asset/js/admin.js', '', '', true);
-wp_enqueue_script('public-js', WPPATT_PLUGIN_URL . 'asset/js/pattpublic.js', '', '', true);
-wp_enqueue_script('modal-js', WPPATT_PLUGIN_URL . 'asset/js/modal.js', '', '', true);
-
-/*https://cdn.datatables.net/responsive/2.2.3/css/responsive.dataTables.min.css
-https://cdn.datatables.net/responsive/2.2.3/js/responsive.dataTables.min.js*/
-
-//Enqueue The scripts 
-
-/*function load_custom_wp_admin_style(){
-    wp_register_script('admin-js', WPPATT_PLUGIN_URL . 'asset/js/admin.js', '', '', true);
-    wp_enqueue_script( 'admin-js' );
-}
-add_action('wp_enqueue_scripts', 'load_custom_wp_admin_style');*/
-
-/*admin_enqueue_scripts('admin-js', WPPATT_PLUGIN_URL . 'asset/js/admin.js', '', '', true);
-admin_enqueue_scripts('public-js', WPPATT_PLUGIN_URL . 'asset/js/public.js', '', '', true);
-admin_enqueue_scripts('modal-js', WPPATT_PLUGIN_URL . 'asset/js/modal.js', '', '', true);*/
-
-/*wp_enqueue_script('dataTables-responsive-js');
-wp_enqueue_script('customScriptDatatables');
-wp_enqueue_style('wpsc-fa-css', WPSC_PLUGIN_URL.'asset/lib/font-awesome/css/all.css?version='.WPSC_VERSION );
-*/
-
+include_once WPPATT_ABSPATH . 'includes/class-wppatt-functions.php';
+$load_styles = new wppatt_Functions();
+$load_styles->addStyles();
 
 $general_appearance = get_option('wpsc_appearance_general_settings');
-$wpsc_appearance_modal_window = get_option('wpsc_modal_window');
+
+$action_default_btn_css = 'background-color:'.$general_appearance['wpsc_default_btn_action_bar_bg_color'].' !important;color:'.$general_appearance['wpsc_default_btn_action_bar_text_color'].' !important;';
+
+$wpsc_appearance_individual_ticket_page = get_option('wpsc_individual_ticket_page');
+
+$edit_btn_css = 'background-color:'.$wpsc_appearance_individual_ticket_page['wpsc_edit_btn_bg_color'].' !important;color:'.$wpsc_appearance_individual_ticket_page['wpsc_edit_btn_text_color'].' !important;border-color:'.$wpsc_appearance_individual_ticket_page['wpsc_edit_btn_border_color'].'!important';
+
 ?>
+
 
 <div class="bootstrap-iso">
   
-  <h3>
-    <?php _e('Boxes','supportcandy');?>
-  </h3>
+  <h3>Box Details</h3>
   
-  <div id="wppatt_boxes_container" class="row" style="border-color:<?php echo $general_appearance['wpsc_action_bar_color']?> !important;"></div>
+ <div id="wpsc_tickets_container" class="row" style="border-color:#1C5D8A !important;">
+
+<div class="row wpsc_tl_action_bar" style="background-color:<?php echo $general_appearance['wpsc_action_bar_color']?> !important;">
   
-  <div id="wpsc_alert_success" class="alert alert-success wpsc_alert" style="display:none;" role="alert">
-    <i class="fa fa-check-circle"></i> <span class="wpsc_alert_text"></span>
+  <div class="col-sm-12">
+    	<button type="button" id="wpsc_individual_ticket_list_btn" onclick="location.href='admin.php?page=wpsc-tickets';" class="btn btn-sm wpsc_action_btn" style="<?php echo $action_default_btn_css?>"><i class="fa fa-list-ul"></i> <?php _e('Ticket List','supportcandy')?></button>
+		<button type="button" class="btn btn-sm wpsc_action_btn" id="wpsc_individual_refresh_btn" onclick="window.location.reload();" style="<?php echo $action_default_btn_css?>"><i class="fas fa-sync-alt"></i> <?php _e('Refresh','supportcandy')?></button>
   </div>
-  
-  <div id="wpsc_alert_error" class="alert alert-danger wpsc_alert" style="display:none;" role="alert">
-    <i class="fa fa-exclamation-triangle"></i> <span class="wpsc_alert_text"></span>
-  </div>
-  
+	
 </div>
 
-<!-- Pop-up snippet start -->
-<div id="wpsc_popup_background" style="display:none;"></div>
-<div id="wpsc_popup_container" style="display:none;">
-  <div class="bootstrap-iso">
-    <div class="row">
-      <div id="wpsc_popup" class="col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-        <div id="wpsc_popup_title" class="row" style="background-color:<?php echo $wpsc_appearance_modal_window['wpsc_header_bg_color']?> !important;color:<?php echo $wpsc_appearance_modal_window['wpsc_header_text_color']?> !important;"><h3><?php _e('Modal Title','supportcandy');?></h3></div>
-        <div id="wpsc_popup_body" class="row"><?php _e('I am body!','supportcandy');?></div>
-        <div id="wpsc_popup_footer" class="row" style="background-color:<?php echo $wpsc_appearance_modal_window['wpsc_footer_bg_color']?> !important;">
-          <button type="button" class="btn wpsc_popup_close" ><?php _e('Close','supportcandy');?></button>
-          <button type="button" class="btn wpsc_popup_action"><?php _e('Save Changes','supportcandy');?></button>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-<!-- Pop-up snippet end -->
+<div class="row" style="background-color:<?php echo $general_appearance['wpsc_bg_color']?> !important;color:<?php echo $general_appearance['wpsc_text_color']?> !important;">
 
+	<div class="col-sm-4 col-md-3 wpsc_sidebar individual_ticket_widget">
 
+							<div class="row" id="wpsc_status_widget" style="background-color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_bg_color']?> !important;color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_text_color']?> !important;border-color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_border_color']?> !important;">
+					      <h4 class="widget_header"><i class="fa fa-filter"></i> Filters
+								</h4>
+								<hr class="widget_divider">
 
-<!-- BEGIN CAR - Added Custom PATT Action -->
-<?php do_action('patt_print_js_tickets_page'); ?>
-<!-- END CAR - Added Custom PATT Action -->
-<?php
-add_action('admin_footer', 'wpsc_page_inline_script');
-global $attrs;
-$attrs = isset($attr['page'])? $attr['page']:'init';
-?>
+	                            <div class="wpsp_sidebar_labels">
+Enter one or more Box IDs:<br />
+         <input type='text' id='searchByBoxID' class="form-control" data-role="tagsinput">
+<br /><br />
+         <select id='searchByProgramOffice'>
+           <option value=''>-- Select Program Office --</option>
+           <option value='OMS'>OMS</option>
+           <option value='R1'>R1</option>
+         </select>
+<br /><br />
+        <select id='searchByDigitizationCenter'>
+           <option value=''>-- Select Digitization Center --</option>
+           <option value='East'>East</option>
+           <option value='West'>West</option>
+         </select>
+
+	                            </div>
+			    		</div>
+	
+	</div>
+	
+  <div class="col-sm-8 col-md-9 wpsc_it_body">
+
+<style>
+.datatable_header {
+background-color: rgb(66, 73, 73) !important; 
+color: rgb(255, 255, 255) !important; 
+}
+ .bootstrap-tagsinput {
+   width: 100%;
+  }
+</style>
+
+<div class="table-responsive" style="overflow-x:auto;">
+<table id="tbl_templates_boxes" class="table table-striped table-bordered" cellspacing="5" cellpadding="5">
+        <thead>
+            <tr>
+                <th class="datatable_header">Box ID</th>
+                <th class="datatable_header">Request ID</th>
+                <th class="datatable_header">Digitization Center</th>
+                <th class="datatable_header">Program Office</th>
+            </tr>
+        </thead>
+    </table>
+<br /><br />
+<link rel="stylesheet" type="text/css" href="<?php echo WPSC_PLUGIN_URL.'asset/lib/DataTables/datatables.min.css';?>"/>
+<script type="text/javascript" src="<?php echo WPSC_PLUGIN_URL.'asset/lib/DataTables/datatables.min.js';?>"></script>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput-typeahead.css" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-tagsinput/0.8.0/bootstrap-tagsinput.min.js" crossorigin="anonymous"></script>
+  
+  
 <script>
-  var wpsc_setting_action = '<?php echo $attrs?>';
+
+jQuery(document).ready(function(){
+  var dataTable = jQuery('#tbl_templates_boxes').DataTable({
+    'processing': true,
+    'serverSide': true,
+    'serverMethod': 'post',
+    'searching': false, // Remove default Search Control
+    'ajax': {
+       'url':'<?php echo WPPATT_PLUGIN_URL; ?>includes/admin/pages/scripts/box_processing.php',
+       'data': function(data){
+          // Read values
+          var boxid = jQuery('#searchByBoxID').val();
+          var po = jQuery('#searchByProgramOffice').val();
+          var dc = jQuery('#searchByDigitizationCenter').val();
+          // Append to data
+          data.searchByBoxID = boxid;
+          data.searchByProgramOffice = po;
+          data.searchByDigitizationCenter = dc;
+       }
+    },
+    'columns': [
+       { data: 'box_id' }, 
+       { data: 'request_id' },
+       { data: 'location' },
+       { data: 'acronym' },
+    ]
+  });
+
+
+  jQuery(document).on('keypress',function(e) {
+    if(e.which == 13) {
+        dataTable.draw();
+    }
+});
+
+  jQuery("input").on('itemRemoved', function(event) {
+    dataTable.draw();
+});
+
+  jQuery("#searchByProgramOffice").change(function(){
+    dataTable.draw();
+});
+
+  jQuery("#searchByDigitizationCenter").change(function(){
+    dataTable.draw();
+});
+  
+});
+
 </script>
 
-<?php
-function wpsc_page_inline_script(){
-  ?>
-  <script>
-  <?php
-     $url_attrs = array();
-     foreach ($_GET as $key => $value) {
-       $url_attrs[] = '"'.$key.'":"'.$value.'"';
-     }
-     $url_attrs = '{'.implode(',',$url_attrs).'}'
-  ?>
-       var attrs = <?php echo $url_attrs?>;
-       jQuery(document).ready(function(){
-         wpsc_init(wpsc_setting_action,attrs);
-       });
-  </script>
-  <?php
-}
-?>
+
+  </div>
+ 
+
+
+</div>
+</div>
+</div>

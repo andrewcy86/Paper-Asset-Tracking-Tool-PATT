55399e2651ec2689cde12a994c56414b6b94b788
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-04-26 08:33:23 -0400

    Create folderfile.php
    Added Folder/File Dashboard.

---
+++ plugins/pattracking/includes/admin/pages/folderfile.php
@@ -0,0 +1,210 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+global $wpdb, $current_user, $wpscfunction;
+
+$GLOBALS['id'] = $_GET['id'];
+
+include_once WPPATT_ABSPATH . 'includes/class-wppatt-functions.php';
+$load_styles = new wppatt_Functions();
+$load_styles->addStyles();
+
+$general_appearance = get_option('wpsc_appearance_general_settings');
+
+$action_default_btn_css = 'background-color:'.$general_appearance['wpsc_default_btn_action_bar_bg_color'].' !important;color:'.$general_appearance['wpsc_default_btn_action_bar_text_color'].' !important;';
+
+$wpsc_appearance_individual_ticket_page = get_option('wpsc_individual_ticket_page');
+
+$edit_btn_css = 'background-color:'.$wpsc_appearance_individual_ticket_page['wpsc_edit_btn_bg_color'].' !important;color:'.$wpsc_appearance_individual_ticket_page['wpsc_edit_btn_text_color'].' !important;border-color:'.$wpsc_appearance_individual_ticket_page['wpsc_edit_btn_border_color'].'!important';
+
+?>
+
+
+<div class="bootstrap-iso">
+  
+  <h3>Folder/File Search</h3>
+  
+ <div id="wpsc_tickets_container" class="row" style="border-color:#1C5D8A !important;">
+
+<div class="row wpsc_tl_action_bar" style="background-color:<?php echo $general_appearance['wpsc_action_bar_color']?> !important;">
+  
+  <div class="col-sm-12">
+    	<button type="button" id="wpsc_individual_ticket_list_btn" onclick="location.href='admin.php?page=wpsc-tickets';" class="btn btn-sm wpsc_action_btn" style="<?php echo $action_default_btn_css?>"><i class="fa fa-list-ul"></i> <?php _e('Ticket List','supportcandy')?></button>
+		<button type="button" class="btn btn-sm wpsc_action_btn" id="wpsc_individual_refresh_btn" onclick="window.location.reload();" style="<?php echo $action_default_btn_css?>"><i class="fas fa-sync-alt"></i> Clear Filters</button>
+  </div>
+	
+</div>
+
+<div class="row" style="background-color:<?php echo $general_appearance['wpsc_bg_color']?> !important;color:<?php echo $general_appearance['wpsc_text_color']?> !important;">
+
+	<div class="col-sm-4 col-md-3 wpsc_sidebar individual_ticket_widget">
+
+							<div class="row" id="wpsc_status_widget" style="background-color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_bg_color']?> !important;color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_text_color']?> !important;border-color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_border_color']?> !important;">
+					      <h4 class="widget_header"><i class="fa fa-filter"></i> Filters
+								</h4>
+								<hr class="widget_divider">
+
+	                            <div class="wpsp_sidebar_labels">
+Enter one or more Box IDs:<br />
+         <input type='text' id='searchByDocID' class="form-control" data-role="tagsinput">
+<br />
+         <select id='searchByProgramOffice'>
+           <option value=''>-- Select Program Office --</option>
+           <option value='OMS'>OMS</option>
+           <option value='R1'>R1</option>
+         </select>
+<br /><br />
+        <select id='searchByDigitizationCenter'>
+           <option value=''>-- Select Digitization Center --</option>
+           <option value='East'>East</option>
+           <option value='West'>West</option>
+         </select>
+
+	                            </div>
+			    		</div>
+	
+	</div>
+	
+  <div class="col-sm-8 col-md-9 wpsc_it_body">
+
+<style>
+.datatable_header {
+background-color: rgb(66, 73, 73) !important; 
+color: rgb(255, 255, 255) !important; 
+}
+
+.bootstrap-tagsinput {
+   width: 100%;
+  }
+
+#searchGeneric {
+    padding: 0 30px !important;
+}
+</style>
+
+<div class="table-responsive" style="overflow-x:auto;">
+<input type="text" id="searchGeneric" class="form-control" name="custom_filter[s]" value="" autocomplete="off" placeholder="Search...">
+<i class="fa fa-search wpsc_search_btn wpsc_search_btn_sarch"></i>
+<br /><br />
+<table id="tbl_templates_boxes" class="table table-striped table-bordered" cellspacing="5" cellpadding="5">
+        <thead>
+            <tr>
+                <th class="datatable_header">Document/File ID</th>
+                <th class="datatable_header">Request ID</th>
+                <th class="datatable_header">Digitization Center</th>
+                <th class="datatable_header">Program Office</th>
+            </tr>
+        </thead>
+    </table>
+<br /><br />
+<link rel="stylesheet" type="text/css" href="<?php echo WPSC_PLUGIN_URL.'asset/lib/DataTables/datatables.min.css';?>"/>
+<script type="text/javascript" src="<?php echo WPSC_PLUGIN_URL.'asset/lib/DataTables/datatables.min.js';?>"></script>
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-tagsinput/1.3.3/jquery.tagsinput.css" crossorigin="anonymous">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-tagsinput/1.3.3/jquery.tagsinput.js" crossorigin="anonymous"></script>
+  
+  
+<script>
+
+jQuery(document).ready(function(){
+
+  var dataTable = jQuery('#tbl_templates_boxes').DataTable({
+    'processing': true,
+    'serverSide': true,
+    'serverMethod': 'post',
+    'searching': false, // Remove default Search Control
+    'ajax': {
+       'url':'<?php echo WPPATT_PLUGIN_URL; ?>includes/admin/pages/scripts/document_processing.php',
+       'data': function(data){
+          // Read values
+          var sg = jQuery('#searchGeneric').val();
+          var docid = jQuery('#searchByDocID').val();
+          var po = jQuery('#searchByProgramOffice').val();
+          var dc = jQuery('#searchByDigitizationCenter').val();
+          // Append to data
+          data.searchGeneric = sg;
+          data.searchByDocID = docid;
+          data.searchByProgramOffice = po;
+          data.searchByDigitizationCenter = dc;
+       }
+    },
+    'columns': [
+       { data: 'folderdocinfo_id' }, 
+       { data: 'request_id' },
+       { data: 'location' },
+       { data: 'acronym' },
+    ]
+  });
+
+  jQuery(document).on('keypress',function(e) {
+    if(e.which == 13) {
+        dataTable.draw();
+    }
+});
+
+  jQuery("#searchByProgramOffice").change(function(){
+    dataTable.draw();
+});
+
+  jQuery("#searchByDigitizationCenter").change(function(){
+    dataTable.draw();
+});
+
+jQuery('#searchGeneric').on('input keyup paste', function () {
+    var hasValue = jQuery.trim(this.value).length;
+    if(hasValue == 0) {
+            dataTable.draw();
+        }
+});
+
+
+		function onAddTag(tag) {
+			dataTable.draw();
+		}
+		function onRemoveTag(tag) {
+			dataTable.draw();
+		}
+
+
+jQuery("#searchByDocID").tagsInput({
+   'defaultText':'',
+   'onAddTag': onAddTag,
+   'onRemoveTag': onRemoveTag,
+   'width':'100%'
+});
+
+jQuery("#searchByDocID_tag").on('paste',function(e){
+    var element=this;
+    setTimeout(function () {
+        var text = jQuery(element).val();
+        var target=jQuery("#searchByBoxID");
+        var tags = (text).split(/[ ,]+/);
+        for (var i = 0, z = tags.length; i<z; i++) {
+              var tag = jQuery.trim(tags[i]);
+              if (!target.tagExist(tag)) {
+                    target.addTag(tag);
+              }
+              else
+              {
+                  jQuery("#searchByDocID_tag").val('');
+              }
+                
+         }
+    }, 0);
+});
+
+
+});
+
+</script>
+
+
+  </div>
+ 
+
+
+</div>
+</div>
+</div>

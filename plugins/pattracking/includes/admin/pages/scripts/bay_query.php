763cd3a3593ec4aba3a27d411d058ca43f2cb189
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-04-29 23:49:15 -0400

    Update bay_query.php

--- plugins/pattracking/includes/admin/pages/scripts/bay_query.php
+++ plugins/pattracking/includes/admin/pages/scripts/bay_query.php
@@ -6,7 +6,7 @@ global $wpdb, $current_user, $wpscfunction;
 include($path.'wp-load.php');
 
 
-    $digitization_center = 'East';
+    //$digitization_center = 'East';
     $digitization_center_bay_aisle_total = 5;
 
     $bay_array = range(1, $digitization_center_bay_aisle_total);
@@ -21,7 +21,7 @@ foreach ($bay_array as $value) {
     $get_available_bay = $wpdb->get_row(
 				"SELECT count(id) as count
 FROM wpqa_wpsc_epa_storage_location
-WHERE aisle = '" . $_GET['aisle_id'] . "' AND bay = '" . $value . "' AND  digitization_center = '" . $digitization_center . "'"
+WHERE aisle = '" . $_GET['aisle_id'] . "' AND bay = '" . $value . "' AND  digitization_center = '" . $_GET['center'] . "'"
 			);
 
     $output_array[$i] = 'Bay #' . $value . ' [' . (20 - $get_available_bay->count) . ' boxes remain]';

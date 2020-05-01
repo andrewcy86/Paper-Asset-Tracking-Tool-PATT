1175e4d52eac53b04c5584d87826b1c7672375f7
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-04-30 23:30:31 -0400

    Create location_update.php
    Update location of box.

---
+++ plugins/pattracking/includes/admin/pages/scripts/location_update.php
@@ -0,0 +1,45 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+global $wpdb, $current_user, $wpscfunction;
+
+
+   $shelf_position = $_POST['postvarspname'];
+ 
+   $array = explode('_', $shelf_position);
+   $shelf = $array[0];
+   $position = $array[1];
+
+   $aisle = $_POST['postvaraname'];
+   $bay = $_POST['postvarbname'];
+   $boxid = $_POST['postvarboxname'];
+   $center = $_POST['postvarcentername'];
+   
+   $center_value = '';
+   
+   if ($center = 'East') {
+     $center_value = 'E';
+   } else if ($center = 'West'){
+     $center_value = 'W';       
+   }
+
+
+			$box_details = $wpdb->get_row(
+"SELECT storage_location_id
+FROM wpqa_wpsc_epa_boxinfo
+WHERE box_id = '" . $boxid . "'"
+			);
+
+			$box_storage_location_id = $box_details->storage_location_id;
+			
+$table_name = 'wpqa_wpsc_epa_storage_location';
+
+$data_update = array('aisle' => $aisle ,'bay'=>$bay,'shelf'=>$shelf,'position'=>$position);
+$data_where = array('id' => $box_storage_location_id);
+$wpdb->update($table_name , $data_update, $data_where);
+
+   echo "Box ID #: " . $boxid . " has been updated. New Location: " .$aisle. "A_" .$bay . "B_" . $shelf ."S_".$position."P_".$center_value;
+
+?>

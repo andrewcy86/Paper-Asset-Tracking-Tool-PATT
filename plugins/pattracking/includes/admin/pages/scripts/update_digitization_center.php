82ee2e8179a3ccfb54e48342d3f18a8714c91d05
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-05-04 10:56:25 -0400

    Create update_digitization_center.php
    Added ability to switch digitization center on a box level manually.

---
+++ plugins/pattracking/includes/admin/pages/scripts/update_digitization_center.php
@@ -0,0 +1,42 @@
+<?php
+
+global $wpdb, $current_user, $wpscfunction;
+
+$path = preg_replace('/wp-content.*$/','',__DIR__);
+include($path.'wp-load.php');
+
+if(isset($_POST['postvarsboxidname'])){
+   $box_id = $_POST['postvarsboxidname'];
+
+   $box_details = $wpdb->get_row(
+"SELECT wpqa_wpsc_epa_boxinfo.storage_location_id as storage_location_id, wpqa_wpsc_epa_boxinfo.id as id, wpqa_wpsc_epa_boxinfo.box_id as box_id, wpqa_wpsc_epa_storage_location.digitization_center as digitization_center
+FROM wpqa_wpsc_epa_boxinfo
+INNER JOIN wpqa_wpsc_epa_storage_location ON wpqa_wpsc_epa_boxinfo.storage_location_id = wpqa_wpsc_epa_storage_location.id
+WHERE wpqa_wpsc_epa_boxinfo.id = '" . $box_id . "'"
+			);
+			
+
+			$box_storage_location_id = $box_details->storage_location_id;
+			$box_storage_digitization_center = $box_details->digitization_center;
+			$box_id_val = $box_details->box_id;
+			
+			$box_storage_digitization_center_val = '';
+
+            if ($box_storage_digitization_center == 'West') {
+            $box_storage_digitization_center_val = 'East';
+            } else if ($box_storage_digitization_center == 'East') {
+            $box_storage_digitization_center_val = 'West';
+            }
+            
+$table_name = 'wpqa_wpsc_epa_storage_location';
+
+
+$data_update = array('digitization_center' => $box_storage_digitization_center_val);
+$data_where = array('id' => $box_storage_location_id);
+$wpdb->update($table_name , $data_update, $data_where);
+
+   echo "Box ID #: " . $box_id_val . " has been updated.\nAssigned Digitization Center: " .$box_storage_digitization_center_val;
+} else {
+   echo "Update not successful.";
+}
+?>

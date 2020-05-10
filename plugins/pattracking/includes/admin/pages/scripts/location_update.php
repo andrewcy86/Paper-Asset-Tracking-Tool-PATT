8f0260618cb5e31a99e69c3fcaee294d01da49e0
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-05-10 12:12:44 -0400

    Update location_update.php
    Incorporate updates to the storage status table.

--- plugins/pattracking/includes/admin/pages/scripts/location_update.php
+++ plugins/pattracking/includes/admin/pages/scripts/location_update.php
@@ -25,7 +25,6 @@ if(isset($_POST['postvarspname']) && isset($_POST['postvaraname']) && isset($_PO
      $center_value = 'W';       
    }
 
-
 			$box_details = $wpdb->get_row(
 "SELECT storage_location_id
 FROM wpqa_wpsc_epa_boxinfo
@@ -34,11 +33,67 @@ WHERE box_id = '" . $boxid . "'"
 
 			$box_storage_location_id = $box_details->storage_location_id;
 			
-$table_name = 'wpqa_wpsc_epa_storage_location';
+// Update storage status add box back from existing shelf location
 
+			$storage_location_details = $wpdb->get_row(
+"SELECT aisle,bay,shelf
+FROM wpqa_wpsc_epa_storage_location
+WHERE id = '" . $box_storage_location_id . "'"
+			);
+
+			$existing_aisle = $storage_location_details->aisle;
+			$existing_bay = $storage_location_details->bay;
+			$existing_shelf = $storage_location_details->shelf;
+			$existing_shelf_id = $existing_aisle.'_'.$existing_bay.'_'.$existing_shelf;
+
+$existing_shelf_update = $wpdb->get_row("
+SELECT remaining
+FROM wpqa_wpsc_epa_storage_status
+WHERE
+shelf_id = '" . $existing_shelf_id . "' AND
+digitization_center = '" . $center . "'
+");
+
+$existing_shelf_update_remaining = $existing_shelf_update->remaining + 1;
+
+				$existing_ss_table_name = 'wpqa_wpsc_epa_storage_status';
+				
+				if ($existing_shelf_update_remaining == 4) {
+				$existing_ss_data_update = array('occupied' => 0, 'remaining' => $existing_shelf_update_remaining);
+				} else {
+				$existing_ss_data_update = array('occupied' => 1, 'remaining' => $existing_shelf_update_remaining);
+				}
+				
+				$existing_ss_data_where = array('shelf_id' => $existing_shelf_id);
+
+				$wpdb->update($existing_ss_table_name, $existing_ss_data_update, $existing_ss_data_where);
+				
+// Update wpqa_wpsc_epa_storage_location with new location that was selected
+
+$table_name = 'wpqa_wpsc_epa_storage_location';
 $data_update = array('aisle' => $aisle ,'bay'=>$bay,'shelf'=>$shelf,'position'=>$position);
 $data_where = array('id' => $box_storage_location_id);
 $wpdb->update($table_name , $data_update, $data_where);
+
+$new_shelf_id_update = $aisle.'_'.$bay.'_'.$shelf;
+
+// Update storage status remove box from new shelf location
+				$new_shelf_update = $wpdb->get_row("
+SELECT remaining
+FROM wpqa_wpsc_epa_storage_status
+WHERE
+shelf_id = '" . $new_shelf_id_update . "' AND
+digitization_center = '" . $center . "'
+");
+
+				$new_shelf_update_remaining = $new_shelf_update->remaining - 1;
+
+				$new_ss_table_name = 'wpqa_wpsc_epa_storage_status';
+				$new_ss_data_update = array('occupied' => 1, 'remaining' => $new_shelf_update_remaining);
+				$new_ss_data_where = array('shelf_id' => $new_shelf_id_update);
+
+				$wpdb->update($new_ss_table_name, $new_ss_data_update, $new_ss_data_where);
+				
 
    echo "Box ID #: " . $boxid . " has been updated. New Location: " .$aisle. "A_" .$bay . "B_" . $shelf ."S_".$position."P_".$center_value;
 } else {

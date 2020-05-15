24c4c510ca7f63d161d328cbcde675d791b064f9
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-05-14 22:12:51 -0400

    Update folder-file-details.php
    Added edit capabilities.

--- plugins/pattracking/includes/admin/pages/folder-file-details.php
+++ plugins/pattracking/includes/admin/pages/folder-file-details.php
@@ -62,14 +62,21 @@ $edit_btn_css = 'background-color:'.$wpsc_appearance_individual_ticket_page['wps
 			$folderfile_grant_number = $folderfile_details->grant_number;
 			$folderfile_file_location = $folderfile_details->file_location;
 			$folderfile_file_name = $folderfile_details->file_name;
-			$box_details = $wpdb->get_row(
+			/*$box_details = $wpdb->get_row(
 				"SELECT wpqa_wpsc_epa_boxinfo.id, wpqa_wpsc_ticket.request_id as request_id, wpqa_wpsc_epa_boxinfo.box_id as box_id, wpqa_wpsc_epa_boxinfo.ticket_id as ticket_id, wpqa_wpsc_epa_boxinfo.index_level as index_level, wpqa_wpsc_epa_boxinfo.location as location, wpqa_wpsc_epa_boxinfo.bay as bay, wpqa_wpsc_epa_boxinfo.shelf as shelf, wpqa_epa_record_schedule.Record_Schedule_Number as rsnum, wpqa_wpsc_epa_program_office.acronym as program_office
 FROM wpqa_wpsc_epa_boxinfo
 INNER JOIN wpqa_epa_record_schedule ON wpqa_wpsc_epa_boxinfo.record_schedule_id = wpqa_epa_record_schedule.id
 INNER JOIN wpqa_wpsc_epa_program_office ON wpqa_wpsc_epa_boxinfo.program_office_id = wpqa_wpsc_epa_program_office.id
 INNER JOIN wpqa_wpsc_ticket ON wpqa_wpsc_epa_boxinfo.ticket_id = wpqa_wpsc_ticket.id
 WHERE wpqa_wpsc_epa_boxinfo.id = '" . $folderfile_boxid . "'"
-			);
+			);*/
+			
+			$box_details = $wpdb->get_row("SELECT wpqa_wpsc_epa_boxinfo.id, wpqa_wpsc_ticket.request_id as request_id, wpqa_wpsc_epa_boxinfo.box_id as box_id, wpqa_wpsc_epa_boxinfo.ticket_id as ticket_id, wpqa_wpsc_epa_folderdocinfo.index_level as index_level, wpqa_wpsc_epa_storage_location.digitization_center as location, wpqa_wpsc_epa_storage_location.aisle as aisle, wpqa_wpsc_epa_storage_location.bay as bay, wpqa_wpsc_epa_storage_location.shelf as shelf, wpqa_wpsc_epa_storage_location.position as position, wpqa_epa_record_schedule.Record_Schedule_Number as rsnum, wpqa_wpsc_epa_program_office.acronym as program_office
+
+FROM wpqa_epa_record_schedule, wpqa_wpsc_epa_boxinfo, wpqa_wpsc_epa_folderdocinfo, wpqa_wpsc_epa_program_office, wpqa_wpsc_epa_storage_location, wpqa_wpsc_ticket
+
+WHERE wpqa_epa_record_schedule.id = wpqa_wpsc_epa_boxinfo.record_schedule_id AND wpqa_wpsc_epa_program_office.id = wpqa_wpsc_epa_boxinfo.program_office_id AND wpqa_wpsc_ticket.id = wpqa_wpsc_epa_boxinfo.ticket_id AND wpqa_wpsc_epa_folderdocinfo.box_id = wpqa_wpsc_epa_boxinfo.id AND wpqa_wpsc_epa_storage_location.id = wpqa_wpsc_epa_boxinfo.storage_location_id
+AND wpqa_wpsc_epa_boxinfo.id = '" . $folderfile_boxid . "'");
 
 			$box_boxid = $box_details->box_id;
 			$box_ticketid = $box_details->ticket_id;
@@ -79,8 +86,10 @@ WHERE wpqa_wpsc_epa_boxinfo.id = '" . $folderfile_boxid . "'"
 			$request_id = substr($box_boxid, 0, 7);
 			$box_il = $box_details->index_level;
 			$box_location = $box_details->location;
+			$box_aisle = $box_details->aisle;
 			$box_bay = $box_details->bay;
 			$box_shelf = $box_details->shelf;
+			$box_position = $box_details->position;
 ?>
 
 <?php
@@ -160,59 +169,126 @@ if (preg_match("/^[0-9]{7}-[0-9]{1,3}-[0-9]{2}-[0-9]{1,3}$/", $GLOBALS['id'])) {
 			}
 ?>
     </div>
-
+<form method="POST" action="#">
 <?php
-
+            $agent_permissions = $wpscfunction->get_current_agent_permissions();
+            $agent_permissions['label'];
+            
 			echo "<strong>Program Office:</strong> " . $box_po . "<br />";
-			
-			echo "<strong>Record Schedule:</strong> " . $box_rs ."<br />";
+            echo "<strong>Record Schedule:</strong> " . $box_rs ."<br />";
+            
+			//only admins/agents have the ability to edit folder/file details			
 			if (!empty($folderfile_title)) {
 				echo "<strong>Title:</strong> " . $folderfile_title . "<br />";
+				if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+			        echo "<input type='text' id='title' placeholder= 'Enter title...'></br></br>";
+			    }
 			}
+			//make a calendar picker
 			if (!empty($folderfile_date)) {
 				echo "<strong>Date:</strong> " . $folderfile_date . "<br />";
+				if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+    			    echo "<input type='date' id='date' placeholder= 'mm/dd/yyyy'></br></br>"; 
+    			}
 			}
 			if (!empty($folderfile_author)) {
 				echo "<strong>Author:</strong> " . $folderfile_author . "<br />";
+				if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+    				echo "<input type='text' id='author' placeholder= 'Enter author...'></br></br>"; 
+    			}
 			}
 			if (!empty($folderfile_record_type)) {
 				echo "<strong>Record Type:</strong> " . $folderfile_record_type . "<br />";
+				if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+    				echo "<input type='text' id='record_type' placeholder= 'Enter record type...'></br></br>"; 
+    			}
 			}
 			if (!empty($folderfile_site_name)) {
 				echo "<strong>Site Name:</strong> " . $folderfile_site_name . "<br />";
+				if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+    				echo "<input type='text' id='site_name' placeholder= 'Enter site name...'></br></br>"; 
+    			}
 			}
 			if (!empty($folderfile_site_id)) {
 				echo "<strong>Site ID #:</strong> " . $folderfile_site_id . "<br />";
+				if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+    				echo "<input type='text' id='site_id' placeholder= 'Enter site ID...'></br></br>"; 
+    			}
 			}
 			if (!empty($folderfile_close_date)) {
 				echo "<strong>Close Date:</strong> " . $folderfile_close_date . "<br />";
+				if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+    				echo "<input type='date' id='close_date' placeholder= 'Enter close date...'></br></br>"; 
+    			}
 			}
 			if (!empty($folderfile_epa_contact_email)) {
 				echo "<strong>Contact Email:</strong> " . $folderfile_epa_contact_email . "<br />";
+				if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+    				echo "<input type='text' id='contact_email' placeholder= 'Enter contact email...'></br></br>"; 
+    			}
 			}
 			if (!empty($folderfile_access_type)) {
 				echo "<strong>Access Type:</strong> " . $folderfile_access_type . "<br />";
+				if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+    				echo "<input type='text' id='access_type' placeholder= 'Enter access type...'></br></br>"; 
+    			}
 			}
 			if (!empty($folderfile_source_format)) {
 				echo "<strong>Source Format:</strong> " . $folderfile_source_format . "<br />";
+				if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+    				echo "<input type='text' id='source_format' placeholder= 'Enter source format...'></br></br>"; 
+    			}
 			}
 			if (!empty($folderfile_rights)) {
 				echo "<strong>Rights:</strong> " . $folderfile_rights . "<br />";
+				if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+    				echo "<input type='text' id='rights' placeholder= 'Enter folder/file rights...'></br></br>"; 
+    			}
 			}
 			if (!empty($folderfile_contract_number)) {
 				echo "<strong>Contract #:</strong> " . $folderfile_contract_number . "<br />";
+				if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+    				echo "<input type='text' id='contract_number' placeholder= 'Enter contract number...'></br></br>"; 
+    			}
 			}
 			if (!empty($folderfile_grant_number)) {
 				echo "<strong>Grant #:</strong> " . $folderfile_grant_number . "<br />";
+				if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+    				echo "<input type='text' id='grant_number' placeholder= 'Enter grant number...'></br></br>"; 
+    			}
 			}
 			
 			if (!empty($folderfile_file_location) || !empty($folderfile_file_name)) {
 				echo '<strong>Link to File:</strong> <a href="' . $folderfile_file_location . '" target="_blank">' . $folderfile_file_name . '</a><br />';
 			}
-
 ?>
 
-<br /><br />
+<br />
+<!--Submit and Reset Buttons-->
+<?php
+if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+{
+    echo '<input type="hidden" id="folderfileid" name="folderfileid" value="'.$GLOBALS['id'].'">';
+    echo '<button type="submit" value="Submit" id="wpsc_create_ticket_submit" class="btn" style="background-color:#419641 !important;color:#FFFFFF !important;border-color:#C3C3C3 !important;" onclick="wpsc_edit_folder_file_details();">Submit</button>';
+    echo '<button type="reset" value ="Reset" id="wpsc_create_ticket_reset" class="btn" style="background-color:#FFFFFF !important;color:#000000 !important;border-color:#C3C3C3 !important;">Reset</button>';
+}
+?>
+</form>
+<br />
+
 <link rel="stylesheet" type="text/css" href="<?php echo WPSC_PLUGIN_URL.'asset/lib/DataTables/datatables.min.css';?>"/>
 <script type="text/javascript" src="<?php echo WPSC_PLUGIN_URL.'asset/lib/DataTables/datatables.min.js';?>"></script>
 <script>
@@ -249,6 +325,31 @@ if (preg_match("/^[0-9]{7}-[0-9]{1,3}-[0-9]{2}-[0-9]{1,3}$/", $GLOBALS['id']) &&
 ?>
 } );
 
+function wpsc_edit_folder_file_details(){		
+		   jQuery.post(
+   '<?php echo WPPATT_PLUGIN_URL; ?>includes/admin/pages/scripts/update_folder_file_details.php',{
+postvarsffid: jQuery("#folderfileid").val(),
+postvarsrs: jQuery("#record_schedule").val(),
+postvarstitle: jQuery("#title").val(),
+postvarsdate: jQuery("#date").val(),
+postvarsauthor: jQuery("#author").val(),
+postvarsrt: jQuery("#record_type").val(),
+postvarssn: jQuery("#site_name").val(),
+postvarssid: jQuery("#site_id").val(),
+postvarscd: jQuery("#close_date").val(),
+postvarsce: jQuery("#contact_email").val(),
+postvarsat: jQuery("#access_type").val(),
+postvarssf: jQuery("#source_format").val(),
+postvarsrights: jQuery("#rights").val(),
+postvarscn: jQuery("#contract_number").val(),
+postvarsgn: jQuery("#grant_number").val()
+}, 
+   function (response) {
+      if(!alert(response)){window.location.reload();}
+      window.location.replace("/wordpress3/wp-admin/admin.php?page=boxdetails&pid=boxsearch&id=<?php echo $box_boxid; ?>");
+   });
+} 
+
 </script>
 
 
@@ -257,7 +358,8 @@ if (preg_match("/^[0-9]{7}-[0-9]{1,3}-[0-9]{2}-[0-9]{1,3}$/", $GLOBALS['id']) &&
 	<div class="col-sm-4 col-md-3 wpsc_sidebar individual_ticket_widget">
 
 							<div class="row" id="wpsc_status_widget" style="background-color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_bg_color']?> !important;color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_text_color']?> !important;border-color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_border_color']?> !important;">
-					      <h4 class="widget_header"><i class="fa fa-arrow-circle-right"></i> Location</h4>
+					      <h4 class="widget_header"><i class="fa fa-arrow-circle-right"></i> Location
+					      </h4>
 								<hr class="widget_divider">
 								<div class="wpsp_sidebar_labels"><strong>Request ID:</strong> 
 	                            <?php 
@@ -283,6 +385,12 @@ if (preg_match("/^[0-9]{7}-[0-9]{1,3}-[0-9]{2}-[0-9]{1,3}$/", $GLOBALS['id']) &&
 	                            echo $box_location . "<br />";
 	                            ?>
 	                            </div>
+								<!--switch out aisle, bay, shelf. and position with storage location table-->
+								<div class="wpsp_sidebar_labels"><strong>Aisle:</strong>
+							    <?php
+	                            echo $box_aisle . "<br />";
+	                            ?>
+	                            </div>
 								<div class="wpsp_sidebar_labels"><strong>Bay:</strong>
 							    <?php
 	                            echo $box_bay . "<br />";
@@ -293,6 +401,11 @@ if (preg_match("/^[0-9]{7}-[0-9]{1,3}-[0-9]{2}-[0-9]{1,3}$/", $GLOBALS['id']) &&
 	                            echo $box_shelf . "<br />";
 	                            ?>
 								</div>
+								<div class="wpsp_sidebar_labels"><strong>Position:</strong>
+							    <?php
+	                            echo $box_position . "<br />";
+	                            ?>
+	                            </div>
 			    		</div>
 	
 	</div>

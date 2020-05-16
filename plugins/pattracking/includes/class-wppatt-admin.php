b8c487078f219313fd3c9d018026bf35a48f3739
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-05-16 12:09:52 -0400

    Update class-wppatt-admin.php
    Added files for modal window support on custom pages and get_folder_file_editor function.

--- plugins/pattracking/includes/class-wppatt-admin.php
+++ plugins/pattracking/includes/class-wppatt-admin.php
@@ -6,7 +6,47 @@ if ( ! defined( 'ABSPATH' ) ) {
 if ( ! class_exists( 'wppatt_Admin' ) ) :
   
   final class wppatt_Admin {
-
+// constructor
+    public function __construct() {
+      add_action( 'admin_enqueue_scripts', array( $this, 'loadScripts') );
+    }
+    
+    // Load admin scripts
+    public function loadScripts(){
+        wp_enqueue_script('jquery');
+        wp_enqueue_script('jquery-ui-core');
+        wp_enqueue_script('jquery-ui-autocomplete', '', array('jquery-ui-widget', 'jquery-ui-position'), '1.8.6');
+        wp_enqueue_script('jquery-ui-sortable');
+        wp_enqueue_script('wp-color-picker');
+        wp_enqueue_style('wp-color-picker');
+        wp_enqueue_script( 'jquery-ui-datepicker' );
+        wp_enqueue_script( 'jquery-ui-slider' );
+        wp_enqueue_editor();
+        //bootstrap
+        wp_enqueue_style('wpsc-bootstrap-css', WPSC_PLUGIN_URL.'asset/css/bootstrap-iso.css?version='.WPSC_VERSION );
+        //Font-Awesom
+        wp_enqueue_style('wpsc-fa-css', WPSC_PLUGIN_URL.'asset/lib/font-awesome/css/all.css?version='.WPSC_VERSION );
+        wp_enqueue_style('wpsc-jquery-ui', WPSC_PLUGIN_URL.'asset/css/jquery-ui.css?version='.WPSC_VERSION );
+        //admin scripts
+        wp_enqueue_script('wpsc-admin', WPSC_PLUGIN_URL.'asset/js/admin.js?version='.WPSC_VERSION, array('jquery'), null, true);
+        wp_enqueue_script('wpsc-public', WPSC_PLUGIN_URL.'asset/js/public.js?version='.WPSC_VERSION, array('jquery'), null, true);
+        wp_enqueue_script('wpsc-modal', WPSC_PLUGIN_URL.'asset/js/modal.js?version='.WPSC_VERSION, array('jquery'), null, true);
+        wp_enqueue_style('wpsc-public-css', WPSC_PLUGIN_URL . 'asset/css/public.css?version='.WPSC_VERSION );
+        wp_enqueue_style('wpsc-admin-css', WPSC_PLUGIN_URL . 'asset/css/admin.css?version='.WPSC_VERSION );
+        wp_enqueue_style('wpsc-modal-css', WPSC_PLUGIN_URL . 'asset/css/modal.css?version='.WPSC_VERSION );
+        //Datetime picker
+        wp_enqueue_script('wpsc-dtp-js', WPSC_PLUGIN_URL.'asset/lib/datetime-picker/jquery-ui-timepicker-addon.js?version='.WPSC_VERSION, array('jquery'), null, true);
+        wp_enqueue_style('wpsc-dtp-css', WPSC_PLUGIN_URL . 'asset/lib/datetime-picker/jquery-ui-timepicker-addon.css?version='.WPSC_VERSION );
+      if(isset($_REQUEST['page'])) :
+        //localize script
+        $loading_html = '<div class="wpsc_loading_icon"><img src="'.WPSC_PLUGIN_URL.'asset/images/ajax-loader@2x.gif"></div>';
+        $localize_script_data = apply_filters( 'wpsc_admin_localize_script', array(
+            'ajax_url'             => admin_url( 'admin-ajax.php' ),
+            'loading_html'         => $loading_html
+        ));
+        wp_localize_script( 'wpsc-admin', 'wpsc_admin', $localize_script_data );
+      endif;
+    }
     // Added function to inject label button
     public function pdflabel_btnAfterClone(){
     include WPPATT_ABSPATH . 'includes/admin/wppatt_get_pdflabel_file.php';    
@@ -135,6 +175,11 @@ switch ($company_name) {
 	  
     public function get_digitization_editor(){    
     include WPPATT_ABSPATH . 'includes/ajax/get_digitization_editor.php';
+    die();
+    }
+
+    public function get_folder_file_editor(){    
+    include WPPATT_ABSPATH . 'includes/ajax/get_folder_file_editor.php';
     die();
     }
   }

b499a25d34fdcb7e9525bd9dc2ba2b08fcddf3da
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-05-16 22:59:05 -0400

    Update box-details.php
    Add editor.

--- plugins/pattracking/includes/admin/pages/box-details.php
+++ plugins/pattracking/includes/admin/pages/box-details.php
@@ -8,17 +8,15 @@ global $wpdb, $current_user, $wpscfunction;
 $GLOBALS['id'] = $_GET['id'];
 $GLOBALS['pid'] = $_GET['pid'];
 
-include_once WPPATT_ABSPATH . 'includes/class-wppatt-functions.php';
-$load_styles = new wppatt_Functions();
-$load_styles->addStyles();
+//include_once WPPATT_ABSPATH . 'includes/class-wppatt-functions.php';
+//$load_styles = new wppatt_Functions();
+//$load_styles->addStyles();
 
 $general_appearance = get_option('wpsc_appearance_general_settings');
 
 $action_default_btn_css = 'background-color:'.$general_appearance['wpsc_default_btn_action_bar_bg_color'].' !important;color:'.$general_appearance['wpsc_default_btn_action_bar_text_color'].' !important;';
 
 $wpsc_appearance_individual_ticket_page = get_option('wpsc_individual_ticket_page');
-
-$edit_btn_css = 'background-color:'.$wpsc_appearance_individual_ticket_page['wpsc_edit_btn_bg_color'].' !important;color:'.$wpsc_appearance_individual_ticket_page['wpsc_edit_btn_text_color'].' !important;border-color:'.$wpsc_appearance_individual_ticket_page['wpsc_edit_btn_border_color'].'!important';
 
 ?>
 
@@ -190,34 +188,59 @@ if (preg_match("/^[0-9]{7}-[0-9]{1,3}$/", $GLOBALS['id']) && $GLOBALS['pid'] == 
   </div>
  
  <?php
-    $location_details = $wpdb->get_row("SELECT acronym, location, shelf, bay, record_schedule_number
-FROM wpqa_wpsc_epa_program_office, wpqa_wpsc_epa_boxinfo, wpqa_epa_record_schedule
-WHERE wpqa_wpsc_epa_program_office.id = wpqa_wpsc_epa_boxinfo.program_office_id AND wpqa_epa_record_schedule.id = wpqa_wpsc_epa_boxinfo.record_schedule_id
+    //get widget fields
+    //$location_details = $wpdb->get_row("SELECT acronym, location, shelf, bay, record_schedule_number FROM wpqa_wpsc_epa_program_office, wpqa_wpsc_epa_boxinfo, wpqa_epa_record_schedule WHERE wpqa_wpsc_epa_program_office.id = wpqa_wpsc_epa_boxinfo.program_office_id AND wpqa_epa_record_schedule.id = wpqa_wpsc_epa_boxinfo.record_schedule_id AND wpqa_wpsc_epa_boxinfo.box_id = '" . $GLOBALS['id'] . "'");
+    $location_details = $wpdb->get_row("SELECT acronym, digitization_center, locations, wpqa_wpsc_epa_storage_location.shelf as shelf, wpqa_wpsc_epa_storage_location.bay as bay, wpqa_wpsc_epa_storage_location.aisle as aisle, wpqa_wpsc_epa_storage_location.position as position, record_schedule_number 
+FROM wpqa_wpsc_epa_program_office, wpqa_wpsc_epa_boxinfo, wpqa_epa_record_schedule, wpqa_wpsc_epa_location_status, wpqa_wpsc_epa_storage_location
+WHERE wpqa_wpsc_epa_program_office.id = wpqa_wpsc_epa_boxinfo.program_office_id AND wpqa_epa_record_schedule.id = wpqa_wpsc_epa_boxinfo.record_schedule_id AND wpqa_wpsc_epa_location_status.id = wpqa_wpsc_epa_boxinfo.location_status_id AND wpqa_wpsc_epa_storage_location.id = wpqa_wpsc_epa_boxinfo.storage_location_id
 AND wpqa_wpsc_epa_boxinfo.box_id = '" . $GLOBALS['id'] . "'");
 
+    
     $location_program_office = $location_details->acronym;
-    $location_digitization_center = $location_details->location;
-    $location_shelf = $location_details->shelf;
+    $location_digitization_center = $location_details->digitization_center;
+    $location_general = $location_details->locations;
+    $location_aisle = $location_details->aisle;
     $location_bay = $location_details->bay;
+    $location_shelf = $location_details->shelf;
+    $location_position = $location_details->position;
     $location_record_schedule = $location_details->record_schedule_number;
+    
  ?>
  
 	<div class="col-sm-4 col-md-3 wpsc_sidebar individual_ticket_widget">
-
-							<div class="row" id="wpsc_status_widget" style="background-color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_bg_color']?> !important;color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_text_color']?> !important;border-color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_border_color']?> !important;">
-					      <h4 class="widget_header"><i class="fa fa-arrow-circle-right"></i> Location
-										<!--<button id="wpsc_individual_change_ticket_status" onclick="wpsc_get_change_ticket_status(<?php echo $ticket_id?>)" class="btn btn-sm wpsc_action_btn" style="<?php echo $edit_btn_css ?>"><i class="fas fa-edit"></i></button>
-								--></h4>
-								<hr class="widget_divider">
-
-	                            <div class="wpsp_sidebar_labels"><strong>Program Office: </strong> <?php echo $location_program_office ?></div>
-	                            <div class="wpsp_sidebar_labels"><strong>Digitization Center: </strong> <?php echo $location_digitization_center ?></div>
-								<div class="wpsp_sidebar_labels"><strong>Bay: </strong> <?php echo $location_bay ?></div>
-								<div class="wpsp_sidebar_labels"><strong>Shelf: </strong> <?php echo $location_shelf ?> </div>
-								<div class="wpsp_sidebar_labels"><strong>Record Schedule: </strong> <?php echo $location_record_schedule ?></div>
-			    		</div>
-	
+		<div class="row" id="wpsc_status_widget" style="background-color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_bg_color']?> !important;color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_text_color']?> !important;border-color:<?php echo $wpsc_appearance_individual_ticket_page['wpsc_ticket_widgets_border_color']?> !important;">
+      <h4 class="widget_header"><i class="fa fa-arrow-circle-right"></i> Location
+			<!--only admins/agents have the ability to edit box details-->
+			<?php
+			    $agent_permissions = $wpscfunction->get_current_agent_permissions();
+                $agent_permissions['label'];
+                if (($agent_permissions['label'] == 'Administrator') || ($agent_permissions['label'] == 'Agent'))
+                {
+                  echo '<button id="wpsc_individual_change_ticket_status" onclick="wpsc_get_box_editor('.$box_id.');" class="btn btn-sm wpsc_action_btn" style="background-color:#FFFFFF !important;color:#000000 !important;border-color:#C3C3C3!important"><i class="fas fa-edit"></i></button>';
+                } 
+			?>
+			
+			</h4>
+			<hr class="widget_divider">
+			
+            <div class="wpsp_sidebar_labels"><strong>Program Office: </strong> <?php echo $location_program_office ?></div>
+            <div class="wpsp_sidebar_labels"><strong>Digitization Center: </strong> <?php echo $location_digitization_center ?></div>
+			<div class="wpsp_sidebar_labels"><strong>Location: </strong> <?php echo $location_general ?></div>
+			<!--Only displays aisle, bay, shelf, and position if location status is 'On Shelf', otherwise only display general location in facility-->
+			<?php
+			    if($location_general == 'On Shelf')
+			    {
+			        echo '<div class="wpsp_sidebar_labels"><strong>Aisle: </strong>' . $location_aisle . '</div>';
+			        echo '<div class="wpsp_sidebar_labels"><strong>Bay: </strong>' . $location_bay . '</div>';
+			        echo '<div class="wpsp_sidebar_labels"><strong>Shelf: </strong>' . $location_shelf . '</div>';
+			        echo '<div class="wpsp_sidebar_labels"><strong>Position: </strong>' . $location_position . '</div>';
+			    }
+			?>
+			
+			<div class="wpsp_sidebar_labels"><strong>Record Schedule: </strong> <?php echo $location_record_schedule ?></div>
 	</div>
+	</div>
+	
 <?php
 } else {
 
@@ -228,3 +251,38 @@ echo '<span style="padding-left: 10px">Please pass a valid Box ID</span>';
 </div>
 </div>
 </div>
+
+<!-- Pop-up snippet start -->
+<div id="wpsc_popup_background" style="display:none;"></div>
+<div id="wpsc_popup_container" style="display:none;">
+  <div class="bootstrap-iso">
+    <div class="row">
+      <div id="wpsc_popup" class="col-xs-10 col-xs-offset-1 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+        <div id="wpsc_popup_title" class="row"><h3>Modal Title</h3></div>
+        <div id="wpsc_popup_body" class="row">I am body!</div>
+        <div id="wpsc_popup_footer" class="row">
+          <button type="button" class="btn wpsc_popup_close"><?php _e('Close','supportcandy');?></button>
+          <button type="button" class="btn wpsc_popup_action"><?php _e('Save Changes','supportcandy');?></button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Pop-up snippet end -->
+
+<script>
+		function wpsc_get_box_editor(box_id){
+
+		  wpsc_modal_open('Edit Box Details');
+		  var data = {
+		    action: 'wpsc_get_box_editor',
+		    box_id: box_id
+		  };
+		  jQuery.post(wpsc_admin.ajax_url, data, function(response_str) {
+		    var response = JSON.parse(response_str);
+		    jQuery('#wpsc_popup_body').html(response.body);
+		    jQuery('#wpsc_popup_footer').html(response.footer);
+		    jQuery('#wpsc_cat_name').focus();
+		  });  
+		}
+</script>

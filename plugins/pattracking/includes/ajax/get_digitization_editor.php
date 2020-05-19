5b0fd241248759347cd31bad8fe7bca62638ed4d
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-05-19 16:23:16 -0400

    Update get_digitization_editor.php
    Disabled selection of blank digitization center.

--- plugins/pattracking/includes/ajax/get_digitization_editor.php
+++ plugins/pattracking/includes/ajax/get_digitization_editor.php
@@ -35,10 +35,11 @@ $patt_ticket_id = $box_details->request_id;
 
   <label for="dc">Choose a Location:</label>
   <select id="dc" name="dc">
-    <option value="East" <?php echo ($digitization_center == 'East') ? 'selected' : ''; ?>>East</option>
-    <option value="East_CUI" <?php echo ($digitization_center == 'East_CUI') ? 'selected' : ''; ?>>East CUI</option>
-    <option value="West" <?php echo ($digitization_center == 'West') ? 'selected' : ''; ?>>West</option>
-    <option value="West_CUI" <?php echo ($digitization_center == 'West_CUI') ? 'selected' : ''; ?>>West CUI</option>
+    <option value="" selected disabled>--Select a Location--</option>
+    <option value="East" <?php echo ($digitization_center == 'East') ? 'disabled' : ''; ?>>East</option>
+    <option value="East_CUI" <?php echo ($digitization_center == 'East_CUI') ? 'disabled' : ''; ?>>East CUI</option>
+    <option value="West" <?php echo ($digitization_center == 'West') ? 'disabled' : ''; ?>>West</option>
+    <option value="West_CUI" <?php echo ($digitization_center == 'West_CUI') ? 'disabled' : ''; ?>>West CUI</option>
   </select>
 
 <?php 
@@ -46,8 +47,17 @@ $body = ob_get_clean();
 ob_start();
 ?>
 <button type="button" class="btn wpsc_popup_close"  style="background-color:<?php echo $wpsc_appearance_modal_window['wpsc_close_button_bg_color']?> !important;color:<?php echo $wpsc_appearance_modal_window['wpsc_close_button_text_color']?> !important;"   onclick="wpsc_modal_close();"><?php _e('Close','wpsc-export-ticket');?></button>
-<button type="button" class="btn wpsc_popup_action" style="background-color:<?php echo $wpsc_appearance_modal_window['wpsc_action_button_bg_color']?> !important;color:<?php echo $wpsc_appearance_modal_window['wpsc_action_button_text_color']?> !important;" onclick="wpsc_get_digitization_editor(<?php echo htmlentities($box_id)?>);"><?php _e('Save','supportcandy');?></button>
+<button type="button" id="digitization_submit" class="btn wpsc_popup_action" style="background-color:<?php echo $wpsc_appearance_modal_window['wpsc_action_button_bg_color']?> !important;color:<?php echo $wpsc_appearance_modal_window['wpsc_action_button_text_color']?> !important;" onclick="wpsc_get_digitization_editor(<?php echo htmlentities($box_id)?>);"><?php _e('Save','supportcandy');?></button>
 <script>
+jQuery("#digitization_submit").hide();
+
+jQuery('#dc').on('click', function () {
+if (jQuery("#dc").val() != '') {
+jQuery("#digitization_submit").show();
+}
+});
+				
+
 function wpsc_get_digitization_editor(box_id){		
 		   jQuery.post(
    '<?php echo WPPATT_PLUGIN_URL; ?>includes/admin/pages/scripts/update_digitization_center.php',{

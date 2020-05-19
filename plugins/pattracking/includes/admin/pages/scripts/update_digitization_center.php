f37b51e4e11bb9b0448b26f20c735a740fac73e2
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-05-18 23:44:39 -0400

    Update update_digitization_center.php
    Update storage status table.

--- plugins/pattracking/includes/admin/pages/scripts/update_digitization_center.php
+++ plugins/pattracking/includes/admin/pages/scripts/update_digitization_center.php
@@ -10,7 +10,15 @@ if(isset($_POST['postvarsboxidname'])){
    $dc = $_POST['postvarsdc'];
 
    $box_details = $wpdb->get_row(
-"SELECT wpqa_wpsc_epa_boxinfo.storage_location_id as storage_location_id, wpqa_wpsc_epa_boxinfo.id as id, wpqa_wpsc_epa_boxinfo.box_id as box_id, wpqa_wpsc_epa_storage_location.digitization_center as digitization_center
+"SELECT 
+wpqa_wpsc_epa_boxinfo.storage_location_id as storage_location_id, 
+wpqa_wpsc_epa_boxinfo.id as id, 
+wpqa_wpsc_epa_boxinfo.box_id as box_id, 
+wpqa_wpsc_epa_storage_location.digitization_center as digitization_center,
+wpqa_wpsc_epa_storage_location.aisle as aisle,
+wpqa_wpsc_epa_storage_location.bay as bay,
+wpqa_wpsc_epa_storage_location.shelf as shelf,
+wpqa_wpsc_epa_storage_location.shelf as position
 FROM wpqa_wpsc_epa_boxinfo
 INNER JOIN wpqa_wpsc_epa_storage_location ON wpqa_wpsc_epa_boxinfo.storage_location_id = wpqa_wpsc_epa_storage_location.id
 WHERE wpqa_wpsc_epa_boxinfo.id = '" . $box_id . "'"
@@ -19,14 +27,39 @@ WHERE wpqa_wpsc_epa_boxinfo.id = '" . $box_id . "'"
 
 			$box_storage_location_id = $box_details->storage_location_id;
 			$box_storage_digitization_center = $box_details->digitization_center;
+			$box_storage_aisle = $box_details->aisle;
+			$box_storage_bay = $box_details->bay;
+			$box_storage_shelf = $box_details->shelf;
+			$box_sotrage_shelf_id = $box_storage_aisle . '_' . $box_storage_bay . '_' . $box_storage_shelf;
 			$box_id_val = $box_details->box_id;
             
-$table_name = 'wpqa_wpsc_epa_storage_location';
+$table_sl = 'wpqa_wpsc_epa_storage_location';
+$sl_update = array('digitization_center' => $dc, 'aisle' => '0' ,'bay'=>'0','shelf'=>'0','position'=>'0');
+$sl_where = array('id' => $box_storage_location_id);
+$wpdb->update($table_sl , $sl_update, $sl_where);
 
+$box_storage_status = $wpdb->get_row(
+"SELECT 
+occupied,
+remaining
+FROM wpqa_wpsc_epa_storage_status
+WHERE shelf_id = '" . $box_sotrage_shelf_id . "'"
+			);
 
-$data_update = array('digitization_center' => $dc, 'aisle' => '0' ,'bay'=>'0','shelf'=>'0','position'=>'0');
-$data_where = array('id' => $box_storage_location_id);
-$wpdb->update($table_name , $data_update, $data_where);
+$box_storage_status_occupied = $box_storage_status->occupied;
+$box_storage_status_remaining = $box_storage_status->remaining + 1;
+
+$table_ss = 'wpqa_wpsc_epa_storage_status';
+$ssr_update = array('remaining' => $box_storage_status_remaining);
+$ssr_where = array('shelf_id' => $box_sotrage_shelf_id, 'digitization_center' => $box_storage_digitization_center);
+$wpdb->update($table_ss , $ssr_update, $ssr_where);
+
+if($box_storage_status_remaining == 4){
+$sso_update = array('occupied' => 0);
+$sso_where = array('shelf_id' => $box_sotrage_shelf_id, 'digitization_center' => $box_storage_digitization_center);
+$wpdb->update($table_ss , $sso_update, $sso_where);
+}
+
 
    echo "Box ID #: " . $box_id_val . " has been updated.\nAssigned Digitization Center: " .$dc;
 } else {

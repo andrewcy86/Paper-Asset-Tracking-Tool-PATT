b81b1388ceccf6eb1f669be5547b7c54b5c1e34c
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-05-19 22:39:19 -0400

    Update set_delete_ticket_permanently.php
    Delete box, location and update status table.

--- plugins/supportcandy/includes/admin/tickets/individual_ticket/set_delete_ticket_permanently.php
+++ plugins/supportcandy/includes/admin/tickets/individual_ticket/set_delete_ticket_permanently.php
@@ -11,6 +11,61 @@ if (!($current_user->ID && $current_user->has_cap('wpsc_agent'))) {
 
 $ticket_id   		   	= isset($_POST['ticket_id']) ? sanitize_text_field($_POST['ticket_id']) : '' ;
 
+// Begin PATT Code
+$get_associated_boxes = $wpdb->get_results("
+SELECT id, storage_location_id FROM wpqa_wpsc_epa_boxinfo 
+WHERE ticket_id = '" . $ticket_id . "'
+");
+
+foreach ($get_associated_boxes as $info) {
+		$associated_box_ids = $info->id;
+		$associated_storage_ids = $info->storage_location_id;
+		
+		$box_details = $wpdb->get_row(
+"SELECT 
+digitization_center,
+aisle,
+bay,
+shelf,
+position
+FROM wpqa_wpsc_epa_storage_location
+WHERE id = '" . $associated_storage_ids . "'"
+			);
+			
+			$box_storage_digitization_center = $box_details->digitization_center;
+			$box_storage_aisle = $box_details->aisle;
+			$box_storage_bay = $box_details->bay;
+			$box_storage_shelf = $box_details->shelf;
+			$box_sotrage_shelf_id = $box_storage_aisle . '_' . $box_storage_bay . '_' . $box_storage_shelf;
+
+$box_storage_status = $wpdb->get_row(
+"SELECT 
+occupied,
+remaining
+FROM wpqa_wpsc_epa_storage_status
+WHERE shelf_id = '" . $box_sotrage_shelf_id . "'"
+			);
+
+$box_storage_status_occupied = $box_storage_status->occupied;
+$box_storage_status_remaining = $box_storage_status->remaining;
+$box_storage_status_remaining_added = $box_storage_status->remaining + 1;
+
+if ($box_storage_status_remaining <= 4) {
+$table_ss = 'wpqa_wpsc_epa_storage_status';
+$ssr_update = array('remaining' => $box_storage_status_remaining_added);
+$ssr_where = array('shelf_id' => $box_sotrage_shelf_id, 'digitization_center' => $box_storage_digitization_center);
+$wpdb->update($table_ss , $ssr_update, $ssr_where);
+
+if($box_storage_status_remaining == 4){
+$sso_update = array('occupied' => 0);
+$sso_where = array('shelf_id' => $box_sotrage_shelf_id, 'digitization_center' => $box_storage_digitization_center);
+$wpdb->update($table_ss , $sso_update, $sso_where);
+}
+		$wpdb->delete($wpdb->prefix.'wpsc_epa_boxinfo', array( 'id' => $associated_box_ids));
+		$wpdb->delete($wpdb->prefix.'wpsc_epa_storage_location', array( 'id' => $associated_storage_ids));
+	}
+// End PATT Code
+
 $wpdb->delete($wpdb->prefix.'wpsc_ticket', array( 'id' => $ticket_id));
  
 $args = array(
@@ -31,5 +86,3 @@ if($ticket_threads) {
 		wp_delete_post($ticket_thread->ID,true);
 	}
 }
-
-

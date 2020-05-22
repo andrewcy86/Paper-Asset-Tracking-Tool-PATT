b03028df98676c24cbb847a5989dab35bf94dfe1
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-05-21 23:42:45 -0400

    Create get_rfid_box_editor.php
    Added multi box editor.

---
+++ plugins/pattracking/includes/ajax/get_rfid_box_editor.php
@@ -0,0 +1,48 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+global $current_user, $wpscfunction, $wpdb;
+
+if (!isset($_SESSION)) {
+    session_start();    
+}
+
+$box_ids = strip_tags($_POST["box_id"]);
+
+//$box_id  = isset($_POST['box_id']) ? sanitize_text_field($_POST['box_id']) : '' ;
+        
+ob_start();
+
+/*
+$box_details = $wpdb->get_row(
+"SELECT wpqa_wpsc_epa_storage_location.digitization_center as digitization_center, wpqa_wpsc_epa_boxinfo.box_id as patt_box_id, wpqa_wpsc_ticket.request_id as request_id
+FROM wpqa_wpsc_epa_boxinfo
+INNER JOIN wpqa_wpsc_epa_storage_location ON wpqa_wpsc_epa_boxinfo.storage_location_id = wpqa_wpsc_epa_storage_location.id
+INNER JOIN wpqa_wpsc_ticket ON wpqa_wpsc_epa_boxinfo.ticket_id = wpqa_wpsc_ticket.id
+WHERE wpqa_wpsc_epa_boxinfo.id = '" . $box_id . "'"
+			);
+
+$digitization_center = $box_details->digitization_center;
+$patt_box_id = $box_details->patt_box_id;
+$patt_ticket_id = $box_details->request_id;
+*/
+
+?>
+
+<button type="button" class="btn btn-sm wpsc_action_btn" id="wpsc_individual_refresh_btn" onclick="window.open('http://086.info/wordpress3/wp-content/plugins/pattracking/includes/ajax/pdf/box_label.php?id=<?php echo $box_ids; ?>');" style="background-color:#337ab7 !important;color:#FFFFFF !important;"><i class="fas fa-tags"></i> Regenerate Box Labels</button>
+
+<?php 
+$body = ob_get_clean();
+ob_start();
+?>
+<button type="button" class="btn wpsc_popup_close"  style="background-color:<?php echo $wpsc_appearance_modal_window['wpsc_close_button_bg_color']?> !important;color:<?php echo $wpsc_appearance_modal_window['wpsc_close_button_text_color']?> !important;"   onclick="wpsc_modal_close();"><?php _e('Close','wpsc-export-ticket');?></button>
+<?php 
+$footer = ob_get_clean();
+
+$output = array(
+  'body'   => $body,
+  'footer' => $footer
+);
+echo json_encode($output);

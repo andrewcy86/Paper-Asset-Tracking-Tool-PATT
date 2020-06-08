4ae2fb645f4df52fbbac3695544543ac629cd289
stephanieschouw <51790259+stephanieschouw@users.noreply.github.com>
2020-06-08 15:37:31 -0400

    Update update_box_details.php
    removed echo statement

--- plugins/pattracking/includes/admin/pages/scripts/update_box_details.php
+++ plugins/pattracking/includes/admin/pages/scripts/update_box_details.php
@@ -20,9 +20,6 @@ $table_name = 'wpqa_wpsc_epa_boxinfo';
 
 //get exact match of program office acronym and take the id from that, insert id into box table
 if(!empty($po)) {
-//get program_office_code from exact match of office_acronym
-//$get_oc = $wpdb->get_row("SELECT office_code FROM wpqa_wpsc_epa_program_office WHERE office_acronym = '" . $po . "'");
-
 //update box table with program office foreign key
 $data_update = array('program_office_id' => $po);
 $data_where = array('id' => $box_id);
@@ -31,17 +28,11 @@ $wpdb->update($table_name , $data_update, $data_where);
 
 //get exact match of record_schedule_number and take the id from that, insert id into box table
 if(!empty($rs)) {
-//$get_rs_id = $wpdb->get_row("SELECT id FROM wpqa_epa_record_schedule WHERE Record_Schedule_Number = '" . $rs . "'");
-
 //update box table with record schedule foreign key
 $data_update = array('record_schedule_id' => $rs);
 $data_where = array('id' => $box_id);
 $wpdb->update($table_name , $data_update, $data_where);
 }
-
- //echo 'Program office: ' . $po;
- //echo 'Record schedule: ' . $rs;
- echo "Box ID #: " . $pattboxid . " has been updated.";
  
 } else {
    echo "Please make an edit.";

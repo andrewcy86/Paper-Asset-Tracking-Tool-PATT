9795d6970c6ecdc115847d2f6f5cea95fe4b8a31
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-06-08 16:17:07 -0400

    Update update_validate.php
    Added multi page support.

--- plugins/pattracking/includes/admin/pages/scripts/update_validate.php
+++ plugins/pattracking/includes/admin/pages/scripts/update_validate.php
@@ -13,11 +13,14 @@ if(
 $folderdocid_string = $_POST['postvarsfolderdocid'];
 $get_userid = $_POST['potvarsuserid'];
 $folderdocid_arr = explode (",", $folderdocid_string);  
+$page_id = $_POST['postvarpid'];
 
 $table_name = 'wpqa_wpsc_epa_folderdocinfo';
 
 $validation_reversal = 0;
 
+
+if($page_id == 'boxdetails') {
 foreach($folderdocid_arr as $key) {    
 $get_validation = $wpdb->get_row("SELECT validation FROM wpqa_wpsc_epa_folderdocinfo WHERE folderdocinfo_id = '".$key."'");
 
@@ -37,6 +40,28 @@ $wpdb->update($table_name , $data_update, $data_where);
 }
 
 //echo $get_validation_val;
+}
+}
+
+if($page_id == 'filedetails') {
+ 
+$get_validation = $wpdb->get_row("SELECT validation FROM wpqa_wpsc_epa_folderdocinfo WHERE folderdocinfo_id = '".$folderdocid_string."'");
+
+$get_validation_val = $get_validation->validation;
+
+if ($get_validation_val == 1){
+$validation_reversal = 1;
+$data_update = array('validation' => 0, 'validation_user_id'=>'');
+$data_where = array('folderdocinfo_id' => $folderdocid_string);
+$wpdb->update($table_name , $data_update, $data_where);
+}
+
+if ($get_validation_val == 0){
+$data_update = array('validation' => 1, 'validation_user_id'=>$get_userid);
+$data_where = array('folderdocinfo_id' => $folderdocid_string);
+$wpdb->update($table_name , $data_update, $data_where);
+}
+
 }
 
 if ($validation_reversal == 1) {

ce760c338f7dcaa95bfaea2ce4a5830687329efc
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-06-08 15:59:02 -0400

    Update unathorize_destruction.php
    Updated to support multiple pages.

--- plugins/pattracking/includes/admin/pages/scripts/unathorize_destruction.php
+++ plugins/pattracking/includes/admin/pages/scripts/unathorize_destruction.php
@@ -12,11 +12,13 @@ if(
 
 $folderdocid_string = $_POST['postvarsfolderdocid'];
 $folderdocid_arr = explode (",", $folderdocid_string);  
+$page_id = $_POST['postvarpid'];
 
 $table_name = 'wpqa_wpsc_epa_folderdocinfo';
 
 $destruction_reversal = 0;
 
+if($page_id == 'boxdetails') {
 foreach($folderdocid_arr as $key) {    
 $get_destruction = $wpdb->get_row("SELECT unauthorized_destruction FROM wpqa_wpsc_epa_folderdocinfo WHERE folderdocinfo_id = '".$key."'");
 
@@ -36,6 +38,29 @@ $wpdb->update($table_name , $data_update, $data_where);
 }
 
 }
+}
+
+if($page_id == 'filedetails') {
+
+$get_destruction = $wpdb->get_row("SELECT unauthorized_destruction FROM wpqa_wpsc_epa_folderdocinfo WHERE folderdocinfo_id = '".$folderdocid_string."'");
+
+$get_destruction_val = $get_destruction->unauthorized_destruction;
+
+if ($get_destruction_val == 1){
+$destruction_reversal = 1;
+$data_update = array('unauthorized_destruction' => 0);
+$data_where = array('folderdocinfo_id' => $folderdocid_string);
+$wpdb->update($table_name , $data_update, $data_where);
+}
+
+if ($get_destruction_val == 0){
+$data_update = array('unauthorized_destruction' => 1);
+$data_where = array('folderdocinfo_id' => $folderdocid_string);
+$wpdb->update($table_name , $data_update, $data_where);
+}
+
+}
+
 
 if ($destruction_reversal == 1) {
 echo "Unauthorized destruction has been updated. A unauthorized destruction has been reversed.";
@@ -45,6 +70,6 @@ echo "Unauthorized destruction has been updated";
 
 
 } else {
-   echo "Please select one or more items to validate.";
+   echo "Please select one or more items to mark as unauthorized destruction.";
 }
 ?>

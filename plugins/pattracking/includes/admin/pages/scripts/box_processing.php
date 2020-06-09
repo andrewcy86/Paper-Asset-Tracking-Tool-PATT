0c4699d65564738b3c73d65c604dc0b4237375f1
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-06-09 11:14:26 -0400

    Update box_processing.php
    Added validation column and unauthorized destruction flag.

--- plugins/pattracking/includes/admin/pages/scripts/box_processing.php
+++ plugins/pattracking/includes/admin/pages/scripts/box_processing.php
@@ -72,13 +72,49 @@ $records = mysqli_fetch_assoc($sel);
 $totalRecordwithFilter = $records['allcount'];
 
 ## Fetch records
-$boxQuery = "SELECT CONCAT('<a href=admin.php?page=boxdetails&pid=boxsearch&id=',a.box_id,'>',a.box_id,'</a>') as box_id, CONCAT('<a href=admin.php?page=wpsc-tickets&id=',b.request_id,'>',b.request_id,'</a>') as request_id, e.name as location, c.office_acronym as acronym 
+$boxQuery = "
+SELECT 
+
+CONCAT('<a href=\"admin.php?page=boxdetails&pid=boxsearch&id=',a.box_id,'\">',a.box_id,'</a>',
+CASE WHEN (SELECT sum(unauthorized_destruction = 1) FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = a.id) > 0 THEN ' <span style=\"font-size: 1em; color: #8b0000;\"><i class=\"fas fa-flag\" title=\"Unauthorized Destruction\"></i></span>'
+ELSE '' 
+END
+) as box_id,
+CONCAT('<a href=admin.php?page=wpsc-tickets&id=',b.request_id,'>',b.request_id,'</a>') as request_id, 
+e.name as location, 
+c.office_acronym as acronym,
+CONCAT(
+CASE 
+
+WHEN (SELECT sum(validation = 1) FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = a.id) != 0 AND (SELECT sum(validation = 1) FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = a.id) < (SELECT count(id) FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = a.id)
+THEN 
+'<span style=\"font-size: 1.3em; color: #FF8C00;\"><i class=\"fas fa-times-circle\" title=\"Not Validated\"></i></span> '
+
+WHEN (SELECT sum(validation = 1) FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = a.id) = 0 AND (SELECT sum(validation = 1) FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = a.id) < (SELECT count(id) FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = a.id)
+THEN 
+'<span style=\"font-size: 1.3em; color: #8b0000;\"><i class=\"fas fa-times-circle\" title=\"Not Validated\"></i></span> '
+
+WHEN (SELECT sum(validation = 1) FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = a.id) = (SELECT count(id) FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = a.id)
+THEN 
+'<span style=\"font-size: 1.3em; color: #008000;\"><i class=\"fas fa-check-circle\" title=\"Validated\"></i></span> '
+
+ELSE '' 
+END,
+
+CASE 
+WHEN (SELECT sum(validation = 1) FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = a.id) != 0
+THEN
+CONCAT((SELECT sum(validation = 1) FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = a.id), '/', (SELECT count(id) FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = a.id))
+ELSE '-'
+END
+) as validation
 FROM wpqa_wpsc_epa_boxinfo as a
 INNER JOIN wpqa_wpsc_ticket as b ON a.ticket_id = b.id
 INNER JOIN wpqa_wpsc_epa_program_office as c ON a.program_office_id = c.office_code
 INNER JOIN wpqa_wpsc_epa_storage_location as d ON a.storage_location_id = d.id
 INNER JOIN wpqa_terms e ON e.term_id = d.digitization_center
 WHERE 1 ".$searchQuery." order by ".$columnName." ".$columnSortOrder." limit ".$row.",".$rowperpage;
+
 $boxRecords = mysqli_query($con, $boxQuery);
 $data = array();
 
@@ -87,7 +123,8 @@ while ($row = mysqli_fetch_assoc($boxRecords)) {
      "box_id"=>$row['box_id'],
      "request_id"=>$row['request_id'],
      "location"=>$row['location'],
-     "acronym"=>$row['acronym']
+     "acronym"=>$row['acronym'],
+     "validation"=>$row['validation'],     
    );
 }
 

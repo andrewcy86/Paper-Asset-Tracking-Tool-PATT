8f5116883cb42b8eca8ff84cc52c5f9e33c26e50
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-06-10 08:59:59 -0400

    Update unathorize_destruction.php
    Add additional check for all flags removed.

--- plugins/pattracking/includes/admin/pages/scripts/unathorize_destruction.php
+++ plugins/pattracking/includes/admin/pages/scripts/unathorize_destruction.php
@@ -12,7 +12,8 @@ if(
 
 $folderdocid_string = $_POST['postvarsfolderdocid'];
 $folderdocid_arr = explode (",", $folderdocid_string);  
-$page_id = $_POST['postvarpid'];
+$page_id = $_POST['postvarpage'];
+$box_id = $_POST['boxid'];
 
 $table_name = 'wpqa_wpsc_epa_folderdocinfo';
 
@@ -61,11 +62,20 @@ $wpdb->update($table_name , $data_update, $data_where);
 
 }
 
+$get_destruction_sum = $wpdb->get_row("SELECT sum(unauthorized_destruction) as sum FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = '".$box_id."'");
 
+$get_destruction_sum_val = $get_destruction_sum->sum;
+
+if ($get_destruction_sum_val > 0) {
+    
 if ($destruction_reversal == 1) {
 echo "Unauthorized destruction has been updated. A unauthorized destruction has been reversed.";
 } else {
 echo "Unauthorized destruction has been updated";
+}
+
+} else {
+echo "All unathorized destruction flags removed";
 }
 
 

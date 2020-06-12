fd4758ac1438d99dc476459f23bbb159151391fb
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-06-11 21:37:56 -0400

    Update update_unauthorize_destruction.php
    Add audit component.

--- plugins/pattracking/includes/admin/pages/scripts/update_unauthorize_destruction.php
+++ plugins/pattracking/includes/admin/pages/scripts/update_unauthorize_destruction.php
@@ -22,8 +22,11 @@ $destruction_reversal = 0;
 if($page_id == 'boxdetails') {
 foreach($folderdocid_arr as $key) {    
 $get_destruction = $wpdb->get_row("SELECT unauthorized_destruction FROM wpqa_wpsc_epa_folderdocinfo WHERE folderdocinfo_id = '".$key."'");
-
 $get_destruction_val = $get_destruction->unauthorized_destruction;
+
+$get_request_id = substr($folderdocid_string, 0, 7);
+$get_ticket_id = $wpdb->get_row("SELECT id FROM wpqa_wpsc_ticket WHERE request_id = '".$get_request_id."'");
+$ticket_id = $get_ticket_id->id;
 
 if ($get_destruction_val == 1){
 $destruction_reversal = 1;
@@ -36,6 +39,7 @@ if ($get_destruction_val == 0){
 $data_update = array('unauthorized_destruction' => 1);
 $data_where = array('folderdocinfo_id' => $key);
 $wpdb->update($table_name , $data_update, $data_where);
+do_action('wpppatt_after_unauthorized_destruction', $ticket_id, $key);
 }
 
 }
@@ -47,6 +51,10 @@ $get_destruction = $wpdb->get_row("SELECT unauthorized_destruction FROM wpqa_wps
 
 $get_destruction_val = $get_destruction->unauthorized_destruction;
 
+$get_request_id = substr($folderdocid_string, 0, 7);
+$get_ticket_id = $wpdb->get_row("SELECT id FROM wpqa_wpsc_ticket WHERE request_id = '".$get_request_id."'");
+$ticket_id = $get_ticket_id->id;
+
 if ($get_destruction_val == 1){
 $destruction_reversal = 1;
 $data_update = array('unauthorized_destruction' => 0);
@@ -58,6 +66,7 @@ if ($get_destruction_val == 0){
 $data_update = array('unauthorized_destruction' => 1);
 $data_where = array('folderdocinfo_id' => $folderdocid_string);
 $wpdb->update($table_name , $data_update, $data_where);
+do_action('wpppatt_after_unauthorized_destruction', $ticket_id, $folderdocid_string);
 }
 
 }
@@ -65,6 +74,7 @@ $wpdb->update($table_name , $data_update, $data_where);
 $get_destruction_sum = $wpdb->get_row("SELECT sum(unauthorized_destruction) as sum FROM wpqa_wpsc_epa_folderdocinfo WHERE box_id = '".$box_id."'");
 
 $get_destruction_sum_val = $get_destruction_sum->sum;
+
 
 if ($page_id == 'boxdetails') {
 if ($get_destruction_sum_val > 0) {

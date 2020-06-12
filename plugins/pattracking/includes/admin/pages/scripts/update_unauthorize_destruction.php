749e1ef3d17e2a5c1c4d6c64217f1d04ba5adce4
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-06-11 23:08:02 -0400

    Update update_unauthorize_destruction.php

--- plugins/pattracking/includes/admin/pages/scripts/update_unauthorize_destruction.php
+++ plugins/pattracking/includes/admin/pages/scripts/update_unauthorize_destruction.php
@@ -24,7 +24,7 @@ foreach($folderdocid_arr as $key) {
 $get_destruction = $wpdb->get_row("SELECT unauthorized_destruction FROM wpqa_wpsc_epa_folderdocinfo WHERE folderdocinfo_id = '".$key."'");
 $get_destruction_val = $get_destruction->unauthorized_destruction;
 
-$get_request_id = substr($folderdocid_string, 0, 7);
+$get_request_id = substr($key, 0, 7);
 $get_ticket_id = $wpdb->get_row("SELECT id FROM wpqa_wpsc_ticket WHERE request_id = '".$get_request_id."'");
 $ticket_id = $get_ticket_id->id;
 
@@ -33,6 +33,7 @@ $destruction_reversal = 1;
 $data_update = array('unauthorized_destruction' => 0);
 $data_where = array('folderdocinfo_id' => $key);
 $wpdb->update($table_name , $data_update, $data_where);
+do_action('wpppatt_after_unauthorized_destruction_unflag', $ticket_id, $key);
 }
 
 if ($get_destruction_val == 0){
@@ -60,6 +61,7 @@ $destruction_reversal = 1;
 $data_update = array('unauthorized_destruction' => 0);
 $data_where = array('folderdocinfo_id' => $folderdocid_string);
 $wpdb->update($table_name , $data_update, $data_where);
+do_action('wpppatt_after_unauthorized_destruction_unflag', $ticket_id, $folderdocid_string);
 }
 
 if ($get_destruction_val == 0){

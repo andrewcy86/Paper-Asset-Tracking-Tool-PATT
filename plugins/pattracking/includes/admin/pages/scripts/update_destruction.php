a3b51d9a2a23c646d5e6ea145beda653b04dee57
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-06-18 12:03:29 -0400

    Update update_destruction.php
    Updated to prevent destruction when status is not destruction approval and validation is not completed.

--- plugins/pattracking/includes/admin/pages/scripts/update_destruction.php
+++ plugins/pattracking/includes/admin/pages/scripts/update_destruction.php
@@ -19,6 +19,25 @@ $table_name = 'wpqa_wpsc_epa_boxinfo';
 $destruction_reversal = 0;
 
 foreach($boxid_arr as $key) {    
+
+
+
+$get_box_db_id = $wpdb->get_row("select id from wpqa_wpsc_epa_boxinfo where box_id = '".$key."'");
+$box_db_id = $get_box_db_id->id;
+
+$get_sum_total = $wpdb->get_row("select count(id) as sum_total_count from wpqa_wpsc_epa_folderdocinfo where box_id = '".$box_db_id."'");
+$sum_total_val = $get_sum_total->sum_total_count;
+
+$get_sum_validation = $wpdb->get_row("select sum(validation) as sum_validation from wpqa_wpsc_epa_folderdocinfo where validation = 1 AND box_id = '".$box_db_id."'");
+$sum_validation = $get_sum_validation->sum_validation;
+
+$get_status = $wpdb->get_row("select b.ticket_status as status from wpqa_wpsc_epa_boxinfo a INNER JOIN wpqa_wpsc_ticket b ON a.ticket_id = b.id where a.id = '".$box_db_id."'");
+$request_status = $get_status->status;
+
+if(($sum_total_val != $sum_validation) || ($request_status != 68)) {
+    echo $key.' : ';
+    echo 'Please ensure all documents are validated and the request status is approved for destruction before destroying the box.'. PHP_EOL;
+} else {
 $get_destruction = $wpdb->get_row("SELECT box_destroyed FROM wpqa_wpsc_epa_boxinfo WHERE box_id = '".$key."'");
 $get_destruction_val = $get_destruction->box_destroyed;
 
@@ -41,13 +60,16 @@ $wpdb->update($table_name , $data_update, $data_where);
 do_action('wpppatt_after_box_destruction', $ticket_id, $key);
 }
 
+if ($destruction_reversal == 1) {
+echo $key.' : ';
+echo "Box destruction has been updated. A box destruction has been reversed.". PHP_EOL;
+} else {
+echo $key.' : ';
+echo "Box destruction has been updated". PHP_EOL;
 }
 
+}
 
-if ($destruction_reversal == 1) {
-echo "Box destruction has been updated. A box destruction has been reversed.";
-} else {
-echo "Box destruction has been updated";
 }
 
 } else {

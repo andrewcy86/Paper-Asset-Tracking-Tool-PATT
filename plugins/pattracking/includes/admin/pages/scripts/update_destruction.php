edfb278e79634e739e3ef5d036f1fce94089e664
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-06-18 22:52:32 -0400

    Update update_destruction.php
    Remove shelf and reset shelf availability.

--- plugins/pattracking/includes/admin/pages/scripts/update_destruction.php
+++ plugins/pattracking/includes/admin/pages/scripts/update_destruction.php
@@ -14,12 +14,14 @@ $boxid_string = $_POST['postvarsboxid'];
 $boxid_arr = explode (",", $boxid_string);  
 $page_id = $_POST['postvarpage'];
 
-$table_name = 'wpqa_wpsc_epa_boxinfo';
+$box_table_name = 'wpqa_wpsc_epa_boxinfo';
 
 $destruction_reversal = 0;
 
-foreach($boxid_arr as $key) {    
+$counter = 0;
 
+foreach($boxid_arr as $key) {    
+$counter++;
 
 
 $get_box_db_id = $wpdb->get_row("select id from wpqa_wpsc_epa_boxinfo where box_id = '".$key."'");
@@ -34,9 +36,52 @@ $sum_validation = $get_sum_validation->sum_validation;
 $get_status = $wpdb->get_row("select b.ticket_status as status from wpqa_wpsc_epa_boxinfo a INNER JOIN wpqa_wpsc_ticket b ON a.ticket_id = b.id where a.id = '".$box_db_id."'");
 $request_status = $get_status->status;
 
+$get_storage_id = $wpdb->get_row("
+SELECT id, storage_location_id FROM wpqa_wpsc_epa_boxinfo 
+WHERE box_id = '" . $key . "'
+");
+$storage_location_id = $get_storage_id->storage_location_id;
+
+$box_details = $wpdb->get_row(
+"SELECT 
+b.digitization_center,
+b.aisle,
+b.bay,
+b.shelf,
+b.position
+FROM wpqa_wpsc_epa_boxinfo a
+INNER JOIN wpqa_wpsc_epa_storage_location b WHERE a.storage_location_id = b.id
+AND a.box_id = '" . $key . "'"
+			);
+			
+			$box_storage_digitization_center = $box_details->digitization_center;
+			$box_storage_aisle = $box_details->aisle;
+			$box_storage_bay = $box_details->bay;
+			$box_storage_shelf = $box_details->shelf;
+			$box_sotrage_shelf_id = $box_storage_aisle . '_' . $box_storage_bay . '_' . $box_storage_shelf;
+
+$box_storage_status = $wpdb->get_row(
+"SELECT 
+occupied,
+remaining
+FROM wpqa_wpsc_epa_storage_status
+WHERE shelf_id = '" . $box_sotrage_shelf_id . "'"
+			);
+
+$box_storage_status_occupied = $box_storage_status->occupied;
+$box_storage_status_remaining = $box_storage_status->remaining;
+$box_storage_status_remaining_added = $box_storage_status->remaining + 1;
+
+
 if(($sum_total_val != $sum_validation) || ($request_status != 68)) {
     echo $key.' : ';
-    echo 'Please ensure all documents are validated and the request status is approved for destruction before destroying the box.'. PHP_EOL;
+    echo 'Please ensure all documents are validated and the request status is approved for destruction before destroying the box.';
+    
+if ($counter > 0) {
+echo PHP_EOL;
+echo PHP_EOL;
+}
+
 } else {
 $get_destruction = $wpdb->get_row("SELECT box_destroyed FROM wpqa_wpsc_epa_boxinfo WHERE box_id = '".$key."'");
 $get_destruction_val = $get_destruction->box_destroyed;
@@ -47,25 +92,55 @@ $ticket_id = $get_ticket_id->id;
 
 if ($get_destruction_val == 1){
 $destruction_reversal = 1;
-$data_update = array('box_destroyed' => 0);
-$data_where = array('box_id' => $key);
-$wpdb->update($table_name , $data_update, $data_where);
+$box_data_update = array('box_destroyed' => 0);
+$box_data_where = array('box_id' => $key);
+$wpdb->update($box_table_name , $box_data_update, $box_data_where);
 do_action('wpppatt_after_box_destruction_unflag', $ticket_id, $key);
 }
 
 if ($get_destruction_val == 0){
-$data_update = array('box_destroyed' => 1);
-$data_where = array('box_id' => $key);
-$wpdb->update($table_name , $data_update, $data_where);
+$box_data_update = array('box_destroyed' => 1);
+$box_data_where = array('box_id' => $key);
+$wpdb->update($box_table_name , $box_data_update, $box_data_where);
+
+//SET SHELF LOCATION TO 0
+$table_sl = 'wpqa_wpsc_epa_storage_location';
+$sl_update = array('digitization_center' => '666','aisle' => '0','bay' => '0','shelf' => '0','position' => '0');
+$sl_where = array('id' => $storage_location_id);
+$wpdb->update($table_sl , $sl_update, $sl_where);
+
+//ADD AVALABILITY TO STORAGE STATUS
+if ($box_storage_status_remaining <= 4) {
+$table_ss = 'wpqa_wpsc_epa_storage_status';
+$ssr_update = array('remaining' => $box_storage_status_remaining_added);
+$ssr_where = array('shelf_id' => $box_sotrage_shelf_id, 'digitization_center' => $box_storage_digitization_center);
+$wpdb->update($table_ss , $ssr_update, $ssr_where);
+}
+
+if($box_storage_status_remaining == 4){
+$sso_update = array('occupied' => 0);
+$sso_where = array('shelf_id' => $box_sotrage_shelf_id, 'digitization_center' => $box_storage_digitization_center);
+$wpdb->update($table_ss , $sso_update, $sso_where);
+}
+
 do_action('wpppatt_after_box_destruction', $ticket_id, $key);
 }
 
 if ($destruction_reversal == 1) {
 echo $key.' : ';
-echo "Box destruction has been updated. A box destruction has been reversed.". PHP_EOL;
+echo "Box destruction has been updated. A box destruction has been reversed.";
+if ($counter > 0) {
+echo PHP_EOL;
+echo PHP_EOL;
+}
+
 } else {
 echo $key.' : ';
-echo "Box destruction has been updated". PHP_EOL;
+echo "Box destruction has been updated";
+if ($counter > 0) {
+echo PHP_EOL;
+echo PHP_EOL;
+}
 }
 
 }

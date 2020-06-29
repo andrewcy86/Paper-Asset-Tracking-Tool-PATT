78453d815a008714f864f923b781384321038085
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-06-29 11:56:16 -0400

    Update box_processing.php
    Fix issue with counts due to -99999.

--- plugins/pattracking/includes/admin/pages/scripts/box_processing.php
+++ plugins/pattracking/includes/admin/pages/scripts/box_processing.php
@@ -57,7 +57,7 @@ if($searchValue != ''){
 }
 
 ## Total number of records without filtering
-$sel = mysqli_query($con,"select count(*) as allcount from wpqa_wpsc_epa_boxinfo");
+$sel = mysqli_query($con,"select count(*) as allcount from wpqa_wpsc_epa_boxinfo WHERE id <> -99999");
 $records = mysqli_fetch_assoc($sel);
 $totalRecords = $records['allcount'];
 

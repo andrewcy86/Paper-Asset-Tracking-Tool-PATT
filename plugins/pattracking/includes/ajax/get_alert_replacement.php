5e993145dde804d086cc4fb65780e7c3694de462
Andrew Yuen <andrewcy86@users.noreply.github.com>
2020-07-01 00:51:56 -0400

    Create get_alert_replacement.php
    Code to replace javascript alert when needed.

---
+++ plugins/pattracking/includes/ajax/get_alert_replacement.php
@@ -0,0 +1,31 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+global $current_user, $wpscfunction, $wpdb;
+
+if (!isset($_SESSION)) {
+    session_start();    
+}
+
+$response_data = $_POST["response_data"];
+
+ob_start();
+
+echo $response_data;
+?>
+
+<?php 
+$body = ob_get_clean();
+ob_start();
+?>
+<button type="button" class="btn wpsc_popup_close"  style="background-color:<?php echo $wpsc_appearance_modal_window['wpsc_close_button_bg_color']?> !important;color:<?php echo $wpsc_appearance_modal_window['wpsc_close_button_text_color']?> !important;"   onclick="wpsc_modal_close();"><?php _e('Close','wpsc-export-ticket');?></button>
+<?php 
+$footer = ob_get_clean();
+
+$output = array(
+  'body'   => $body,
+  'footer' => $footer
+);
+echo json_encode($output);
